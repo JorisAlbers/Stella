f3d061ce692e522ef21d84eb800c0cd5fafedad2
JorisAlbers <joris_albers@hotmail.com>
2018-11-04 18:15:24 +0100

    Adds SlidingPatternAnimator

---
+++ StellaLib.Test/Animation/Animators/TestSlidingPatternAnimator.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using StellaLib.Animation;
+using StellaLib.Animation.Animators;
+
+namespace StellaLib.Test.Animation.Animators
+{
+    [TestFixture]
+    public class TestSlidingPatternAnimator
+    {
+        [Test]
+        public void Create_ThreeColors_CreatesCorrectFrameSet()
+        {
+            // Setup
+            Color[] pattern = new Color[]{
+                 new Color{ Red = 1, Green = 2, Blue = 3 },
+                 new Color{ Red = 4, Green = 5, Blue = 6 },
+                 new Color{ Red = 7, Green = 8, Blue = 9 },
+                 };
+            int lengthStrip = 7;
+            SlidingPatternAnimator animator = new SlidingPatternAnimator(lengthStrip,pattern);
+
+            // Expected
+            Color expectedColor1 = new Color{ Red = 1, Green = 2, Blue = 3 };
+            Color expectedColor2 = new Color{ Red = 4, Green = 5, Blue = 6 };
+            Color expectedColor3 = new Color{ Red = 7, Green = 8, Blue = 9 };
+
+            FrameSet frameSet = animator.Create();
+
+            //Assert
+            Assert.AreEqual(3,frameSet.Count);
+            //Frame 1
+            Frame frame1 = frameSet[0];
+            Assert.AreEqual(lengthStrip, frame1.Count);
+            Assert.AreEqual(frame1[0].Color, expectedColor1);
+            Assert.AreEqual(frame1[1].Color, expectedColor2);
+            Assert.AreEqual(frame1[2].Color, expectedColor3);
+            Assert.AreEqual(frame1[3].Color, expectedColor1);
+            Assert.AreEqual(frame1[4].Color, expectedColor2);
+            Assert.AreEqual(frame1[5].Color, expectedColor3);
+            Assert.AreEqual(frame1[6].Color, expectedColor1);
+            //Frame 2
+            Frame frame2 = frameSet[1];
+            Assert.AreEqual(lengthStrip, frame2.Count);
+            Assert.AreEqual(frame2[0].Color, expectedColor2);
+            Assert.AreEqual(frame2[1].Color, expectedColor3);
+            Assert.AreEqual(frame2[2].Color, expectedColor1);
+            Assert.AreEqual(frame2[3].Color, expectedColor2);
+            Assert.AreEqual(frame2[4].Color, expectedColor3);
+            Assert.AreEqual(frame2[5].Color, expectedColor1);
+            Assert.AreEqual(frame2[6].Color, expectedColor2);
+            //Frame 3
+            Frame frame3 = frameSet[2];
+            Assert.AreEqual(lengthStrip, frame3.Count);
+            Assert.AreEqual(frame3[0].Color, expectedColor3);
+            Assert.AreEqual(frame3[1].Color, expectedColor1);
+            Assert.AreEqual(frame3[2].Color, expectedColor2);
+            Assert.AreEqual(frame3[3].Color, expectedColor3);
+            Assert.AreEqual(frame3[4].Color, expectedColor1);
+            Assert.AreEqual(frame3[5].Color, expectedColor2);
+            Assert.AreEqual(frame3[6].Color, expectedColor3);
+        }
+    }
+}
---
+++ StellaLib/Animation/Animators/SlidingPatternAnimator.cs
@@ -0,0 +1,34 @@
+namespace StellaLib.Animation.Animators
+{
+    /// <summary>
+    /// Repeats a pattern over the ledstrip and moves the start point of the pattern each
+    /// frame by +1.
+    /// </summary>
+    public class SlidingPatternAnimator : IAnimator
+    {
+        private Color[] _pattern;
+        private int _stripLength;
+
+        public SlidingPatternAnimator(int stripLength, Color[] pattern)
+        {
+            _stripLength = stripLength;
+            _pattern = pattern;
+        }
+
+        public FrameSet Create()
+        {
+            FrameSet frameSet = new FrameSet();
+            for (int i = 0; i < _pattern.Length; i++)
+            {
+                Frame frame = new Frame();
+                int startIndex = i;
+                for (int j = 0; j < _stripLength; j++) 
+                {
+                    frame.Add(new PixelInstruction(){ Index = (uint)j, Color = _pattern[(j + startIndex) % (_pattern.Length)] });
+                }
+                frameSet.Add(frame);
+            }
+            return frameSet;
+        }
+    }
+}
