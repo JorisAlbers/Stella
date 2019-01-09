7f794290cffaf404bfbaab28811dedf4456ba160
JorisAlbers <joris_albers@hotmail.com>
2019-01-09 21:24:12 +0100

    Added StellaClient

---
+++ StellaClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StellaClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of StellaClient");
+        }
+    }
+}
