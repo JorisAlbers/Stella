2412c0635ba6de8d51dd6a9e68bd13d608033bf9
JorisAlbers <joris_albers@hotmail.com>
2019-01-15 19:57:44 +0100

    Implemented IList<Frame> instead of IEnumerable

--- StellaLib/Animation/FrameSet.cs
+++ StellaLib/Animation/FrameSet.cs
@@ -7,7 +7,7 @@ namespace StellaLib.Animation
     /// Collection of Frames.
     /// </summary>
     /// <typeparam name="Frame"></typeparam>
-    public class FrameSet : IEnumerable<Frame>
+    public class FrameSet : IList<Frame>
     {
         private List<Frame> _frames;
 
@@ -16,26 +16,62 @@ namespace StellaLib.Animation
             _frames = new List<Frame>();
         }
 
-        public void Add(Frame frame)
-        {
-            _frames.Add(frame);
-        }
-        
-        public Frame this[int index] =>  _frames[index];
+        # region IList interface
+        public Frame this[int index] { get => ((IList<Frame>)_frames)[index]; set => ((IList<Frame>)_frames)[index] = value; }
 
-        public int Count => _frames.Count;
+        public int Count => ((IList<Frame>)_frames).Count;
+
+        public bool IsReadOnly => ((IList<Frame>)_frames).IsReadOnly;
+
+        public void Add(Frame item)
+        {
+            ((IList<Frame>)_frames).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((IList<Frame>)_frames).Clear();
+        }
+
+        public bool Contains(Frame item)
+        {
+            return ((IList<Frame>)_frames).Contains(item);
+        }
+
+        public void CopyTo(Frame[] array, int arrayIndex)
+        {
+            ((IList<Frame>)_frames).CopyTo(array, arrayIndex);
+        }
 
         public IEnumerator<Frame> GetEnumerator()
         {
-             for (int i = 0; i < _frames.Count; i++) 
-             {
-                 yield return _frames[i];
-             }
+            return ((IList<Frame>)_frames).GetEnumerator();
+        }
+
+        public int IndexOf(Frame item)
+        {
+            return ((IList<Frame>)_frames).IndexOf(item);
+        }
+
+        public void Insert(int index, Frame item)
+        {
+            ((IList<Frame>)_frames).Insert(index, item);
+        }
+
+        public bool Remove(Frame item)
+        {
+            return ((IList<Frame>)_frames).Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList<Frame>)_frames).RemoveAt(index);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this.GetEnumerator();
+            return ((IList<Frame>)_frames).GetEnumerator();
         }
+        #endregion
     }
 }
