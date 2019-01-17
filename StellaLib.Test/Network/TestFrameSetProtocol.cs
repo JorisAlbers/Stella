02524b1b355226706109bfbb78825e48eb52f252
JorisAlbers <joris_albers@hotmail.com>
2019-01-17 20:22:23 +0100

    Added FrameSetProtocol

---
+++ StellaLib.Test/Network/TestFrameSetProtocol.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Drawing;
+using NUnit.Framework;
+using StellaLib.Animation;
+using StellaLib.Network;
+
+namespace StellaLib.Test.Network
+{
+    [TestFixture]
+    public class TestFrameSetProtocol
+    {
+        [Test]
+        public void SerializeFrame_FrameSet_CreatesCorrectByteArray()
+        {
+            Frame frame = new Frame
+            { 
+                new PixelInstruction{ Index = 1,   Color = Color.FromArgb(1,2,3)},
+                new PixelInstruction{ Index = 2,   Color = Color.FromArgb(4,5,6)},
+                new PixelInstruction{ Index = 10,  Color = Color.FromArgb(7,8,9)}
+            };
+            byte[] expectedBytes = new byte[sizeof(int)+ sizeof(int)+3 + sizeof(int)+3 + sizeof(int)+3];
+            BitConverter.GetBytes(3).CopyTo(expectedBytes,0); 
+            // instruction 1
+            BitConverter.GetBytes(1).CopyTo(expectedBytes,4);
+            expectedBytes[8] = (byte)1;
+            expectedBytes[9] = (byte)2;
+            expectedBytes[10] = (byte)3;
+            // instruction 1
+            BitConverter.GetBytes(2).CopyTo(expectedBytes,11);
+            expectedBytes[15]  = (byte)4;
+            expectedBytes[16] = (byte)5;
+            expectedBytes[17] = (byte)6;
+            // instruction 1
+            BitConverter.GetBytes(10).CopyTo(expectedBytes,18);
+            expectedBytes[22] = (byte)7;
+            expectedBytes[23] = (byte)8;
+            expectedBytes[24] = (byte)9;
+
+            Assert.AreEqual(expectedBytes, FrameSetProtocol.SerializeFrame(frame));
+        }
+    }
+}
---
+++ StellaLib/Network/FrameSetProtocol.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using StellaLib.Animation;
+
+namespace StellaLib.Network
+{
+    public class FrameSetProtocol
+    {
+        public static byte[] SerializeFrame(Frame frame)
+        {
+            int headerBytesNeeded   = sizeof(int);      // count of pixel changes in the frame
+            int bytesPerFrameNeeded = sizeof(int) + 3;  // index + byte for each color
+            int bytesNeeded = headerBytesNeeded + frame.Count * bytesPerFrameNeeded;
+
+            byte[] buffer = new byte[bytesNeeded];
+            BitConverter.GetBytes(frame.Count).CopyTo(buffer,0);
+            for(int i = 0; i< frame.Count;i++)
+            {
+                int bufferStartIndex = headerBytesNeeded +  i * bytesPerFrameNeeded;
+
+                //Index
+                BitConverter.GetBytes(frame[i].Index).CopyTo(buffer,  bufferStartIndex);
+                bufferStartIndex += sizeof(int);
+                buffer[bufferStartIndex] = frame[i].Color.R;
+                bufferStartIndex += 1;
+                buffer[bufferStartIndex] = frame[i].Color.G;
+                bufferStartIndex += 1;
+                buffer[bufferStartIndex] = frame[i].Color.B;
+            }
+            return buffer;
+        }
+
+        private FrameSet _frameSet;
+        private List<byte> _buffer;
+
+        // Creates bytes
+        public FrameSetProtocol()
+        {
+            _frameSet = new FrameSet();
+            _buffer = new List<byte>(PacketProtocol.BUFFER_SIZE);
+        }
+
+        
+    }
+}
