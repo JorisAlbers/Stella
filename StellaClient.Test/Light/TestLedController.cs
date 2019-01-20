bd81e4a0a3675ae370b5c3c557e1a3cc3d203934
JorisAlbers <joris_albers@hotmail.com>
2019-01-20 21:38:21 +0100

    Added unit test for call to ILEDStrip.SetLedColor()

--- StellaClient.Test/Light/TestLedController.cs
+++ StellaClient.Test/Light/TestLedController.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.Threading;
 using Moq;
 using NUnit.Framework;
 using rpi_ws281x;
@@ -11,6 +12,31 @@ namespace StellaClient.Test.Light
     public class TestLedController
     {
         [Test]
+        public void AddFrame_Frame_FrameGetsDrawnToStrip()
+        {
+            Frame frame = new Frame{ new PixelInstruction{ Index = 20, Color = Color.FromArgb(10,20,30)}};
+            var mock = new Mock<ILEDStrip>();
+
+            int index = -1;
+            Color color;
+            mock.Setup(x=> x.SetLEDColor(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<Color>())).Callback<int,int,Color>((i,j,c) =>
+            {
+                index = j;
+                color = c;
+            });
+
+            LedController controller = new LedController(mock.Object);
+            controller.Run();
+            controller.AddFrame(frame);
+            Thread.Sleep(2000); // Async hack
+            controller.Dispose();
+
+            Assert.AreEqual(20,index);
+            Assert.AreEqual(Color.FromArgb(10,20,30),color);
+            
+        }
+
+        [Test]
         public void AddFrame_Frame_AddsFrameToTheQueue()
         {
             Frame frame = new Frame{ new PixelInstruction{ Index = 20, Color = Color.FromArgb(10,20,30)}};
@@ -20,5 +46,6 @@ namespace StellaClient.Test.Light
             controller.AddFrame(frame);
             Assert.AreEqual(1, controller.FramesInBuffer);
         }
+        
     }
 }
