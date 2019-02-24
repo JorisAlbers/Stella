f4464210573a3833275e6e26f27fc46701218f37
JorisAlbers <joris_albers@hotmail.com>
2019-02-24 15:00:37 +0100

    Added TimeIsNTPSynched

--- StellaClient/Time/ISystemTimeSetter.cs
+++ StellaClient/Time/ISystemTimeSetter.cs
@@ -6,6 +6,12 @@ namespace StellaClient.Time
         /// The number of ticks, positive or negative, to add to the current system time.
         /// </summary>
         /// <param name="ticks"></param>
-         void AdjustTimeWithTicks(long ticks);
+        void AdjustTimeWithTicks(long ticks);
+
+        /// <summary>
+        /// Checks if the system time is already sync via a NTP server.
+        /// </summary>
+        /// <returns></returns>
+        bool TimeIsNTPSynced();
     }
 }
--- StellaClient/Time/LinuxTimeSetter.cs
+++ StellaClient/Time/LinuxTimeSetter.cs
@@ -16,10 +16,35 @@ namespace StellaClient.Time
             // TODO add miliseconds int miliseconds = TimeSpan.TicksPerMillisecond
 
             // date --set="2011-12-07 01:20:15.962"  && date --rfc-3339=ns
-            string returnValue = RunBashCommand($"date -s @{unixTimeSeconds}");
+            if(!RunBashCommand($"date -s @{unixTimeSeconds}", out string returnMessage))
+            {
+                throw new Exception($"Failed to set the time. Return message of process: {returnMessage}");
+            }
         }
 
-        private string RunBashCommand(string cmd)
+        public bool TimeIsNTPSynced()
+        {
+            if(RunBashCommand("timedatectl status", out string returnMessage))
+            {
+                // Parse the message.
+                // We are looking for "NTP synchronized : "
+                string[] split = returnMessage.Split('\n');
+                for(int i=0; i< split.Length;i++)
+                {
+                    string[] lineSplit = split[i].Split(':');
+                    Console.WriteLine($"{lineSplit[0]} - {lineSplit[1]}");
+                    if(lineSplit[0].Contains("NTP synchronized"))
+                    {
+                        return lineSplit[1].Contains("yes");
+                    }
+                }
+                throw new Exception($"Failed to parse the timedatectl status message. Return message of process: {returnMessage}");
+            }
+
+            throw new Exception($"Failed to get the timedatectl status. Return message of process: {returnMessage}");
+        }
+
+        private bool RunBashCommand(string cmd, out string returnMessage)
         {
             string escapedArgs = cmd.Replace("\"", "\\\"");
 
@@ -35,9 +60,9 @@ namespace StellaClient.Time
                 }
             };
             process.Start();
-            string result = process.StandardOutput.ReadToEnd();
+            returnMessage = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
-            return result;
+            return process.ExitCode == 0;
         }
     }
 }
