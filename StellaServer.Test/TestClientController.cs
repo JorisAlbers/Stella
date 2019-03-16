5dc9b4f9075d9ee31b85c258a8c60aba3e5e2127
JorisAlbers <joris_albers@hotmail.com>
2019-03-17 00:19:38 +0100

    Added Server_OnAnimationRequestReceived()

--- StellaServer.Test/TestClientController.cs
+++ StellaServer.Test/TestClientController.cs
@@ -5,6 +5,7 @@ using StellaLib.Network.Protocol.Animation;
 using Moq;
 using System;
 using StellaLib.Network;
+using StellaLib.Network.Protocol;
 
 namespace StellaServer.Test
 {
@@ -42,8 +43,67 @@ namespace StellaServer.Test
 
             //ASSERT
             Assert.AreEqual(expectedID,id);
-            Assert.AreEqual(expectedMessageType,expectedMessageType);
+            Assert.AreEqual(expectedMessageType,messageType);
             Assert.AreEqual(expectedBytes,bytes);
+        }
+
+        [Test]
+        public void AnimationRequestReceived_TwoFramesRequested_SendsTheFrames()
+        {
+            //SETUP
+            DateTime timeStamp = DateTime.Now;
+            string expectedID = "ID1";
+            MessageType expectedMessageType = MessageType.Animation_Request;
+            FrameSet frameSet = new FrameSet(timeStamp);
+
+            frameSet.Add(new Frame(0,100){new PixelInstruction(10,1,2,3)});
+            frameSet.Add(new Frame(1,200){new PixelInstruction(20,4,5,6)});
+
+            byte[] expectedBytes1 = FrameProtocol.SerializeFrame(frameSet[0],PacketProtocol.MAX_MESSAGE_SIZE)[0];
+            byte[] expectedBytes2 = FrameProtocol.SerializeFrame(frameSet[1],PacketProtocol.MAX_MESSAGE_SIZE)[0];
+            var mock = new Mock<IServer>();
+            mock.SetupGet(x=> x.ConnectedClients).Returns(new string[]{expectedID});
+
+            string id1 = null,  id2 = null;
+            MessageType messageType1 = MessageType.Unknown, messageType2 = MessageType.Unknown;
+            byte[] bytes1 = null, bytes2 = null;
+            int callCount = 0;
+
+            mock.Setup(x=> x.SendMessageToClient(It.IsAny<string>(),
+                                                 It.IsAny<MessageType>(),
+                                                 It.IsAny<byte[]>()))
+                                                 .Callback<string,MessageType,byte[]>((i,t,b) =>
+            {
+                if(t == MessageType.Animation_Request && callCount++ == 0)
+                {
+                    id1 = i;
+                    messageType1 = t;
+                    bytes1 = b;
+                }
+                else
+                {
+                    id2 = i;
+                    messageType2 = t;
+                    bytes2 = b;
+                }
+                
+            });
+
+            //EXECUTE
+            ClientController controller = new ClientController(mock.Object);
+            controller.StartAnimation(frameSet);
+
+            mock.Raise(m => m.AnimationRequestReceived += null, new AnimationRequestEventArgs(expectedID,0,2));
+
+            //ASSERT
+            // Frame1
+            Assert.AreEqual(expectedID,id1);
+            Assert.AreEqual(expectedMessageType,messageType1);
+            Assert.AreEqual(expectedBytes1,bytes1);
+            // Frame 2            
+            Assert.AreEqual(expectedID,id2);
+            Assert.AreEqual(expectedMessageType,messageType2);
+            Assert.AreEqual(expectedBytes2,bytes2);
         }
     }
 }
--- StellaServer/ClientController.cs
+++ StellaServer/ClientController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using StellaLib.Animation;
 using StellaLib.Network;
+using StellaLib.Network.Protocol;
 using StellaLib.Network.Protocol.Animation;
 using StellaServer.Network;
 
@@ -8,10 +11,12 @@ namespace StellaServer
     public class ClientController
     {
         private readonly IServer _server;
-        private FrameSet _currentlyPlayingFrameSet;
+        private FrameSet _frameSet;
+        private object _frameSetLock = new object();
         public ClientController(IServer server)
         {
             _server = server;
+            _server.AnimationRequestReceived += Server_OnAnimationRequestReceived;
         }
         
         // TODO add a Animation object. The animation object contains
@@ -20,21 +25,57 @@ namespace StellaServer
         {
             // TODO for now, all clients get the same animation
             // Should be separately settable in the animation object.
-            _currentlyPlayingFrameSet = frameSet;
-            
-            // Send the ANIMATION_START message to all clients.
-            // When the client has received the message and has prepared the animation buffer,
-            // The client will request the first frames to fill it's buffer.
-            byte[] message = FrameSetProtocol.Serialize(frameSet);
-            foreach(string clientID in _server.ConnectedClients)
+            lock(_frameSetLock)
             {
-                _server.SendMessageToClient(clientID,MessageType.Animation_Start,message);
+                _frameSet = frameSet;
+                
+                // Send the ANIMATION_START message to all clients.
+                // When the client has received the message and has prepared the animation buffer,
+                // The client will request the first frames to fill it's buffer.
+                byte[] message = FrameSetProtocol.Serialize(frameSet);
+                foreach(string clientID in _server.ConnectedClients)
+                {
+                    _server.SendMessageToClient(clientID,MessageType.Animation_Start,message);
+                }
             }
+            
         }
 
+        private void Server_OnAnimationRequestReceived(object sender, AnimationRequestEventArgs e)
+        {
+            lock(_frameSetLock)
+            {
+                if(_frameSet == null)
+                {
+                    Console.WriteLine($"Not sending frames to client {e.ClientID} as there is no frame set on display.");
+                    return;
+                }
 
+                // Create packages
+                List<byte[]> packages = new List<byte[]>();
+                for (int i = e.StartIndex; i < e.StartIndex + e.Count; i++)
+                {
+                    if(i > _frameSet.Count)
+                    {
+                        break;
+                    }
 
+                    byte[][] bytes = FrameProtocol.SerializeFrame(_frameSet[i], PacketProtocol.MAX_MESSAGE_SIZE);
+                    packages.AddRange(bytes);
+                }
 
+                // Send packages
+                if(packages.Count < 1)
+                {
+                    Console.WriteLine($"Not sending frames to client {e.ClientID} as there are not any frames in the frameSet from {e.StartIndex} till {e.StartIndex + e.Count}.");
+                    return;
+                }
 
+                foreach(byte[] packet in packages)
+                {
+                    _server.SendMessageToClient(e.ClientID,MessageType.Animation_Request,packet);
+                }
+            }
+        }
     }
 }
