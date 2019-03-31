42d3fa005fdc0d956f69a20449dae33bb97c7f39
JorisAlbers <joris_albers@hotmail.com>
2019-03-31 11:58:01 +0200

    Added more ledcontroller tests

--- EndToEndTests/ClientTests.cs
+++ EndToEndTests/ClientTests.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Net;
+using System.Threading;
 
 namespace EndToEndTests
 {
@@ -60,11 +61,13 @@ namespace EndToEndTests
                 Console.WriteLine("LedController tests");
                 Console.WriteLine("m - Moving dot");
                 Console.WriteLine("s - Color switch");
+                Console.WriteLine("slow - Slow Color switch");
                 Console.WriteLine("p - Pending FramSet test");
+                Console.WriteLine("clear - clear the ledstrip");
                 Console.WriteLine("q - back");
 
                 string input = Console.ReadLine();
-                switch(input)
+                switch (input)
                 {
                     case "q":
                         run = false;
@@ -73,19 +76,21 @@ namespace EndToEndTests
                     {
                         int waitMS = 20;
                         Color color = Color.BlanchedAlmond;
-                        Color clearColor = Color.FromArgb(0,0,0);
+                        Color clearColor = Color.FromArgb(0, 0, 0);
 
                         List<Frame> frames = new List<Frame>();
-                        frames.Add(new Frame(0,waitMS){new PixelInstruction{ Index = 0, Color = color}});
-                        for(int i=1; i < ledCount;i++)
+                        frames.Add(new Frame(0, 0) {new PixelInstruction {Index = 0, Color = color}});
+                        for (int i = 1; i < ledCount; i++)
                         {
-                            frames.Add(new Frame(i,waitMS)
+                            frames.Add(new Frame(i, i * waitMS)
                             {
-                                new PixelInstruction{ Index = (uint)i -1, Color = clearColor},
-                                new PixelInstruction{ Index = (uint)i, Color = color}
+                                new PixelInstruction {Index = (uint) i - 1, Color = clearColor},
+                                new PixelInstruction {Index = (uint) i, Color = color}
                             });
                         }
-                        frames.Add(new Frame(ledCount,waitMS){new PixelInstruction{ Index = (uint)ledCount-1, Color = clearColor}});
+
+                        frames.Add(new Frame(ledCount, ledCount * waitMS)
+                            {new PixelInstruction {Index = (uint) ledCount - 1, Color = clearColor}});
 
                         LedController controller = new LedController(ledstrip);
                         //LedController controller = new LedController(new FakeLedStrip());
@@ -99,6 +104,7 @@ namespace EndToEndTests
                         break;
                     case "s":
                     {
+                        FrameSetMetadata metadata = new FrameSetMetadata(DateTime.Now);
                         int waitMS = 100;
 
                         Color[] colors = new Color[]
@@ -107,68 +113,138 @@ namespace EndToEndTests
                             Color.Green,
                         };
                         List<Frame> frames = new List<Frame>();
-                        for(int i = 0; i< 100; i++)
+                        for (int i = 0; i < 100; i++)
                         {
-                            for(int j=0;j < colors.Length;j++)
+                            for (int j = 0; j < colors.Length; j++)
                             {
-                                Frame frame = new Frame(j,waitMS);
-                                for(uint k = 0; k<ledCount;k++)
+                                Frame frame = new Frame(j, i * waitMS);
+                                for (uint k = 0; k < ledCount; k++)
                                 {
-                                    frame.Add(new PixelInstruction{Index = k, Color = colors[j]});
+                                    frame.Add(new PixelInstruction {Index = k, Color = colors[j]});
                                 }
+
                                 frames.Add(frame);
                             }
                         }
+
                         LedController controller = new LedController(ledstrip);
 
                         controller.Run();
-                        controller.PrepareNextFrameSet(new FrameSetMetadata(DateTime.Now));
+                        controller.PrepareNextFrameSet(metadata);
                         controller.AddFrames(frames);
                         Console.WriteLine("Press enter to quit");
                         Console.ReadLine();
                         controller.Dispose();
                     }
-                    break;
+                        break;
                     case "p":
                     {
-                        int waitMS = 500;
+                        int waitMS = 1000;
                         //Animation 1
                         Color[] colors1 = new Color[]
                         {
                             Color.Red,
                             Color.Green,
                         };
+                        int timeStampRelative = 0;
                         List<Frame> frames1 = new List<Frame>();
                         for (int i = 0; i < 100; i++)
                         {
                             for (int j = 0; j < colors1.Length; j++)
                             {
-                                Frame frame = new Frame(j, waitMS);
+                                Frame frame = new Frame(j, timeStampRelative);
                                 for (uint k = 0; k < ledCount; k++)
                                 {
-                                    frame.Add(new PixelInstruction { Index = k, Color = colors1[j] });
+                                    frame.Add(new PixelInstruction {Index = k, Color = colors1[j]});
                                 }
+
                                 frames1.Add(frame);
+                                timeStampRelative += waitMS;
                             }
                         }
-                            // Animation 2
+
+                        // Animation 2
                         Color[] colors2 = new Color[]
                         {
                             Color.Blue,
                             Color.White,
                         };
                         List<Frame> frames2 = new List<Frame>();
+                        timeStampRelative = 0;
                         for (int i = 0; i < 100; i++)
                         {
                             for (int j = 0; j < colors2.Length; j++)
                             {
-                                Frame frame = new Frame(j, waitMS);
+                                Frame frame = new Frame(j, timeStampRelative);
                                 for (uint k = 0; k < ledCount; k++)
                                 {
-                                    frame.Add(new PixelInstruction { Index = k, Color = colors2[j] });
+                                    frame.Add(new PixelInstruction {Index = k, Color = colors2[j]});
                                 }
+
                                 frames2.Add(frame);
+                                timeStampRelative += waitMS;
                             }
+                        }
+
+                        LedController controller = new LedController(ledstrip);
+
+                        controller.Run();
+                        DateTime start = DateTime.Now;
+                        controller.PrepareNextFrameSet(new FrameSetMetadata(start));
+                        controller.AddFrames(frames1);
+
+                        string userInput;
+                        bool animationToggle = false;
+                        Console.WriteLine("Press enter to prepare with next animation. q to quit ");
+                        while ((userInput = Console.ReadLine()) != "q")
+                        {
+
+                            controller.PrepareNextFrameSet(
+                                new FrameSetMetadata(DateTime.Now + TimeSpan.FromSeconds(1)));
+
+                            if (animationToggle = !animationToggle)
+                            {
+                                controller.AddFrames(frames1);
+                            }
+                            else
+                            {
+                                controller.AddFrames(frames2);
+                            }
+                        }
+
+                        controller.Dispose();
+                    }
+                        break;
+                    case "slow":
+                    {
+                        int waitMS = 5000;
+                        //Animation 1
+                        Color[] colors1 = new Color[]
+                        {
+                            Color.LightGoldenrodYellow,
+                            Color.LightGreen,
+                        };
+                        int timeStampRelative = 0;
+                        List<Frame> frames1 = new List<Frame>();
+                        int index = 0;
+                        for (int i = 0; i < 100; i++)
+                        {
+                            for (int j = 0; j < colors1.Length; j++)
+                            {
+                                Frame frame = new Frame(index++, timeStampRelative);
+                                for (uint k = 0; k < ledCount; k++)
+                                {
+                                    frame.Add(new PixelInstruction {Index = k, Color = colors1[j]});
+                                }
+
+                                frames1.Add(frame);
+                                timeStampRelative += waitMS;
+                            }
+                        }
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.Out.WriteLine($"TSR: {frames1[i].TimeStampRelative} Color: {frames1[i][0].Color}");
                         }
 
                         LedController controller = new LedController(ledstrip);
@@ -182,22 +258,49 @@ namespace EndToEndTests
                         Console.WriteLine("Press enter to prepare with next animation. q to quit ");
                         while ((userInput = Console.ReadLine()) != "q")
                         {
-                            controller.PrepareNextFrameSet(new FrameSetMetadata(DateTime.Now + TimeSpan.FromSeconds(1)));
-                            
-                            if (animationToggle =! animationToggle)
+                            controller.PrepareNextFrameSet(
+                                new FrameSetMetadata(DateTime.Now + TimeSpan.FromSeconds(1)));
+
+                            if (animationToggle = !animationToggle)
                             {
                                 controller.AddFrames(frames1);
                             }
-                            else
-                            {
-                                controller.AddFrames(frames2);
-                            }
                         }
-                        controller.Dispose();
-                        }
-                    break;
 
-                    default:
+                        controller.Dispose();
+                    }
+                        break;
+                    case "clear":
+                    {
+                        int waitMS = 0;
+                        //Animation 1
+                        Color[] colors1 = new Color[]
+                        {
+                            Color.FromArgb(0,0,0,0),
+                        };
+                        int timeStampRelative = 0;
+                        List<Frame> frames1 = new List<Frame>();
+                        Frame frame = new Frame(0, timeStampRelative);
+                        for (uint k = 0; k < ledCount; k++)
+                        {
+                            frame.Add(new PixelInstruction { Index = k, Color = colors1[0] });
+                        }
+                        frames1.Add(frame);
+                        LedController controller = new LedController(ledstrip);
+
+                        controller.Run();
+                        controller.PrepareNextFrameSet(new FrameSetMetadata(DateTime.Now));
+                        controller.AddFrames(frames1);
+
+                        Thread.Sleep(100);
+
+                        controller.Dispose();
+                    }
+                
+
+                break;
+
+                default:
                         Console.WriteLine($"Unknown command {input}");
                         break;
                 }
