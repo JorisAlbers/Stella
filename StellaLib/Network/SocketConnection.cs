8bcfdb8479a2f73fc6c411a15b108be2bc36d773
JorisAlbers <joris_albers@hotmail.com>
2019-04-02 19:58:22 +0200

    Removed unused usings

--- StellaLib/Network/SocketConnection.cs
+++ StellaLib/Network/SocketConnection.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace StellaLib.Network
 {
