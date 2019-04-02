fe187624d24f27ad6de8a53dcf6721c8eed5dc72
JorisAlbers <joris_albers@hotmail.com>
2019-04-02 20:50:23 +0200

    Added SendCallback_SocketException_InvokesOnDisconnect

--- StellaLib.Test/Network/TestSocketConnectionController.cs
+++ StellaLib.Test/Network/TestSocketConnectionController.cs
@@ -41,5 +41,42 @@ namespace StellaLib.Test.Network
             Assert.AreEqual(expectedMessage,receivedMessage);
         }
 
+        [Test]
+        public void SendCallback_SocketException_InvokesOnDisconnect()
+        {
+            MessageType messageType = MessageType.Init;
+            byte[] message = Encoding.ASCII.GetBytes("test message");
+            byte[] expectedMessage = PacketProtocol.WrapMessage(messageType, message);
+
+            var mock = new Mock<ISocketConnection>();
+            mock.Setup(x => x.Connected).Returns(true);
+
+            // Chain the BeginSend to the EndSend SendCallBack function
+            mock.Setup(x => x.BeginSend(
+                    It.IsAny<byte[]>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<SocketFlags>(),
+                    It.IsAny<AsyncCallback>(),
+                    It.IsAny<object>()))
+                .Callback<byte[], int, int, SocketFlags, AsyncCallback, object>((data, offset, size, socketFlags, asyncCallback, state) =>
+                {
+                    asyncCallback.Invoke(null);
+                });
+
+            mock.Setup(x => x.EndSend(It.IsAny<IAsyncResult>())).Throws<SocketException>();
+            // Listen to the Disconnect event
+            bool disconnectInvoked = false;
+            SocketConnectionController controller = new SocketConnectionController(mock.Object);
+            controller.Disconnect += (sender, args) => disconnectInvoked = true;
+
+            // Run
+            controller.Start();
+            controller.Send(messageType, message);
+            Assert.IsTrue(disconnectInvoked);
+        }
+
+
+
     }
 }
