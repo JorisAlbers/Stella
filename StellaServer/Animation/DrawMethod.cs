5596c0e1374c15ef74e111ba49432b769dc5f619
JorisAlbers <joris_albers@hotmail.com>
2019-04-23 22:33:30 +0200

    Added DrawMethod enum

---
+++ StellaServer/Animation/DrawMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StellaServer.Animation
+{
+    /// <summary>
+    /// The different draw methods available.
+    /// </summary>
+    enum DrawMethod
+    {
+        /// <summary> Not set. </summary>
+        Unknown,
+
+        /// <summary> Repeats a pattern over the LedStrip and moves the start point of the pattern each frame by +1. </summary>
+        SlidingPattern,
+
+        /// <summary> Repeats one or multiple pattern(s) over the length of the LedStrip.
+        /// Each frame is a Color[] pattern repeated over the LedStrip. </summary>
+        RepeatingPattern,
+
+        /// <summary> Moves a pattern over the led strip from the start of the led strip till the end. </summary>
+        MovingPattern
+    }
+}
