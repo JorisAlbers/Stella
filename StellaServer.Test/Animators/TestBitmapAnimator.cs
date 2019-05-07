8c4729d7ab9e603f68682883a45d18aedb6edfc4
JorisAlbers <joris_albers@hotmail.com>
2019-05-07 21:40:37 +0200

    Added BitmapAnimator

---
+++ StellaServer.Test/Animators/TestBitmapAnimator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using NUnit.Framework;
+using StellaLib.Animation;
+using StellaServer.Animation.Animators;
+
+
+namespace StellaServer.Test.Animators
+{
+    [TestFixture]
+    class TestBitmapAnimator
+    {
+        [Test]
+        public void Create_BitmapWithOneRow_CreatesAnimationWithOneFrame()
+        {
+            // SETUP
+            int width = 3;
+            int height = 1;
+
+            int stripLength = 3;
+            int frameWaitMs = 100;
+
+            Color expectedColor1 = Color.FromArgb(255, 255, 0, 0);
+            Color expectedColor2 = Color.FromArgb(255, 0, 255, 0);
+            Color expectedColor3 = Color.FromArgb(255, 0, 0, 255);
+
+           
+            Bitmap bitmap = new Bitmap(width,height);
+            bitmap.SetPixel(0,0,expectedColor1);
+            bitmap.SetPixel(1,0,expectedColor2);
+            bitmap.SetPixel(2,0,expectedColor3);
+            
+            // ACT
+            BitmapAnimator animator = new BitmapAnimator(stripLength,frameWaitMs,bitmap);
+            List<Frame> frames = animator.Create();
+
+            // ASSERT
+            Assert.AreEqual(height, frames.Count);
+            Assert.AreEqual(expectedColor1, frames[0][0].Color);
+            Assert.AreEqual(expectedColor2, frames[0][1].Color);
+            Assert.AreEqual(expectedColor3, frames[0][2].Color);
+        }
+
+        [Test]
+        public void Create_BitmapWithTwoRow_CreatesAnimationWithOneFrame()
+        {
+            // SETUP
+            int width = 3;
+            int height = 2;
+
+            int stripLength = 3;
+            int frameWaitMs = 100;
+
+            Color expectedColor1 = Color.FromArgb(255, 255, 0, 0);
+            Color expectedColor2 = Color.FromArgb(255, 0, 255, 0);
+            Color expectedColor3 = Color.FromArgb(255, 0, 0, 255);
+
+            Color expectedColor4 = Color.FromArgb(255, 255, 255, 255);
+            Color expectedColor5 =  Color.FromArgb(255, 255, 0, 255);
+            Color expectedColor6 =  Color.FromArgb(255, 0, 0, 0);
+
+
+            Bitmap bitmap = new Bitmap(width, height);
+            bitmap.SetPixel(0, 0, expectedColor1);
+            bitmap.SetPixel(1, 0, expectedColor2);
+            bitmap.SetPixel(2, 0, expectedColor3);
+            bitmap.SetPixel(0, 1, expectedColor4);
+            bitmap.SetPixel(1, 1, expectedColor5);
+            bitmap.SetPixel(2, 1, expectedColor6);
+
+            // ACT
+            BitmapAnimator animator = new BitmapAnimator(stripLength, frameWaitMs, bitmap);
+            List<Frame> frames = animator.Create();
+
+            // ASSERT
+            Assert.AreEqual(height, frames.Count);
+            // row 1
+            Assert.AreEqual(3, frames[0].Count);
+            Assert.AreEqual(expectedColor1, frames[0][0].Color);
+            Assert.AreEqual(expectedColor2, frames[0][1].Color);
+            Assert.AreEqual(expectedColor3, frames[0][2].Color);
+            // row 2
+            Assert.AreEqual(3,frames[1].Count);
+            Assert.AreEqual(expectedColor4, frames[1][0].Color);
+            Assert.AreEqual(expectedColor5, frames[1][1].Color);
+            Assert.AreEqual(expectedColor6, frames[1][2].Color);
+
+        }
+
+    }
+}
---
+++ StellaServer/Animation/Animators/BitmapAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using StellaLib.Animation;
+
+namespace StellaServer.Animation.Animators
+{
+    /// <summary>
+    /// Reads a bitmap.
+    /// Each row in the bitmap is a frame.
+    /// </summary>
+    public class BitmapAnimator : IAnimator
+    {
+        private readonly int _stripLength;
+        private readonly int _frameWaitMs;
+        private readonly Bitmap _bitmap;
+
+        public BitmapAnimator(int stripLength, int frameWaitMS, Bitmap bitmap)
+        {
+            _stripLength = stripLength;
+            _frameWaitMs = frameWaitMS;
+            _bitmap = bitmap;
+        }
+
+        public List<Frame> Create()
+        {
+            int width = Math.Min(_bitmap.Width, _stripLength);
+
+            List<Frame> frames = new List<Frame>();
+            for (int i = 0; i < _bitmap.Height; i++)
+            {
+                Frame frame = new Frame(i, i*_frameWaitMs);
+                for (int j = 0; j < width; j++)
+                {
+                    Color color = _bitmap.GetPixel(j, i);
+                    frame.Add(new PixelInstruction((uint)j,color));
+                }
+                frames.Add(frame);
+            }
+
+            return frames;
+        }
+    }
+}
