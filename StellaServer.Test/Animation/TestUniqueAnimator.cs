50e1d58cb3083e5d70c4f2725c29c8695ca94786
JorisAlbers <joris_albers@hotmail.com>
2019-05-08 22:30:33 +0200

    Added Test to check if the looping of the animation works

--- StellaServer.Test/Animation/TestUniqueAnimator.cs
+++ StellaServer.Test/Animation/TestUniqueAnimator.cs
@@ -43,38 +43,7 @@ namespace StellaServer.Test.Animation
             // Pi 1
             Assert.AreEqual(expectedFrame1, animator.GetNextFrame(0));
             // Pi 2
-            Assert.AreEqual(expectedFrame1, animator.GetNextFrame(1));
-        }
-
-        [Test]
-        public void GetNextFrame_MoreThanFramesRequestedThanDrawerReturns_Loops()
-        {
-            Frame expectedFrame1 = new Frame(0, 0)
-            {
-                new PixelInstruction(1, 10, 20, 30)
-            };
-            Frame expectedFrame2 = new Frame(1, 1)
-            {
-                new PixelInstruction(1, 10, 20, 30)
-            };
-
-
-            List<Frame> frames = new List<Frame>()
-            {
-                expectedFrame1,
-                expectedFrame2
-            };
-
-            var drawer = new Mock<IDrawer>();
-            drawer.Setup(x => x.Create()).Returns(frames);
-
-            MirroringAnimator animator = new MirroringAnimator(drawer.Object, 1);
-
-            // Pi 1
-            Assert.AreEqual(expectedFrame1, animator.GetNextFrame(0));
-            Assert.AreEqual(expectedFrame2, animator.GetNextFrame(0));
-            //  loop
-            Assert.AreEqual(expectedFrame1, animator.GetNextFrame(0));
+            Assert.AreEqual(expectedFrame2, animator.GetNextFrame(1));
         }
     }
 }
