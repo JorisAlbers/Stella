f08c43771637d04417fca27848e787c929344aa3
JorisAlbers <joris_albers@hotmail.com>
2019-05-08 20:52:30 +0200

    Added IAnimator

---
+++ StellaServer/Animation/IAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StellaLib.Animation;
+
+namespace StellaServer.Animation
+{
+    /// <summary>
+    /// A animator decides what the next frame of a pi looks like. 
+    /// </summary>
+    interface IAnimator
+    {
+        /// <summary> Get the next frame for a pi at a certain index</summary>
+        /// <param name="piIndex"></param>
+        /// <returns></returns>
+        Frame GetNextFrame(int piIndex);
+
+    }
+}
