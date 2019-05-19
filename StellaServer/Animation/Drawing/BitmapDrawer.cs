77c2199e2fab1410258adc1d019cb8d234229779
JorisAlbers <joris_albers@hotmail.com>
2019-05-19 17:16:14 +0200

    Bugfix: The bitmapDrawer now converts the image to be threadsafe

--- StellaServer/Animation/Drawing/BitmapDrawer.cs
+++ StellaServer/Animation/Drawing/BitmapDrawer.cs
@@ -12,32 +12,43 @@ namespace StellaServer.Animation.Drawing
     /// </summary>
     public class BitmapDrawer : IDrawer
     {
-        private readonly int _stripLength;
         private readonly int _frameWaitMs;
-        private readonly Bitmap _bitmap;
+        private readonly List<PixelInstruction>[] _frames;
 
         public BitmapDrawer(int stripLength, int frameWaitMS, Bitmap bitmap)
         {
-            _stripLength = stripLength;
+            // Convert the bitmap to frames
+            int width = Math.Min(bitmap.Width, stripLength);
+            _frames = new List<PixelInstruction>[bitmap.Height];
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                List<PixelInstruction> frame = new List<PixelInstruction>();
+                for (int j = 0; j < width; j++)
+                {
+                    Color color = bitmap.GetPixel(j, i);
+                    frame.Add(new PixelInstruction((uint)j, color));
+                }
+
+                _frames[i] = frame;
+            }
+
             _frameWaitMs = frameWaitMS;
-            _bitmap = bitmap;
         }
 
         public IEnumerator<Frame> GetEnumerator()
         {
-            int width = Math.Min(_bitmap.Width, _stripLength);
+            int frameIndex = 0;
+            int relativeTimestamp = 0;
             while (true)
             {
-                for (int i = 0; i < _bitmap.Height; i++)
+                for (int i = 0; i < _frames.Length; i++)
                 {
-                    Frame frame = new Frame(i, i * _frameWaitMs);
-                    for (int j = 0; j < width; j++)
-                    {
-                        Color color = _bitmap.GetPixel(j, i);
-                        frame.Add(new PixelInstruction((uint)j, color));
-                    }
-
+                    Frame frame = new Frame(frameIndex, relativeTimestamp);
+                    frame.AddRange(_frames[i]);
                     yield return frame;
+
+                    frameIndex++;
+                    relativeTimestamp += _frameWaitMs;
                 }
             }
         }
