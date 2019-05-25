3b33a0e7f7d3ab4b3806c37eb4627142414e4fe4
JorisAlbers <joris_albers@hotmail.com>
2019-05-25 20:05:28 +0200

    Added TwoForwardMappingsWithoutSections test

--- StellaServer.Test/Animation/Mapping/TestPiMaskCalculator.cs
+++ StellaServer.Test/Animation/Mapping/TestPiMaskCalculator.cs
@@ -90,5 +90,47 @@ namespace StellaServer.Test.Animation.Mapping
             Assert.AreEqual(500, item5.PixelIndex);
         }
 
+        [Test]
+        public void Calculate_TwoForwardMappingsWithoutSections_CorrectlyCreatesMask()
+        {
+            int expectedPiIndex1 = 9;
+            int expectedPiIndex2 = 1;
+            int expectedLength = 2;
+            int expectedStartIndex1 = 0;
+            int expectedStartIndex2 = 0;
+            int expectedStartIndexOnPi1 = 500;
+            int expectedStartIndexOnPi2 = 100;
+
+            List<PiMapping> mappings = new List<PiMapping>()
+            {
+                new PiMapping(expectedPiIndex1,expectedStartIndex1,expectedLength,expectedStartIndexOnPi1,new int[]{},false),
+                new PiMapping(expectedPiIndex2,expectedStartIndex2,expectedLength,expectedStartIndexOnPi2, new int[]{},false)
+            };
+
+            PiMaskCalculator maskCalculator = new PiMaskCalculator(mappings);
+            List<PiMaskItem> piMaskItems = maskCalculator.Calculate();
+
+            Assert.AreEqual(4, piMaskItems.Count);
+
+            // Mapping 1
+            // Item 1
+            PiMaskItem item1 = piMaskItems[0];
+            Assert.AreEqual(expectedPiIndex1, item1.PiIndex);
+            Assert.AreEqual(500, item1.PixelIndex);
+            // Item 2
+            PiMaskItem item2 = piMaskItems[1];
+            Assert.AreEqual(expectedPiIndex1, item2.PiIndex);
+            Assert.AreEqual(501, item2.PixelIndex);
+
+            // Mapping 2
+            PiMaskItem item3 = piMaskItems[2];
+            Assert.AreEqual(expectedPiIndex2, item3.PiIndex);
+            Assert.AreEqual(100, item3.PixelIndex);
+            // Item 2
+            PiMaskItem item4 = piMaskItems[3];
+            Assert.AreEqual(expectedPiIndex2, item4.PiIndex);
+            Assert.AreEqual(101, item4.PixelIndex);
+        }
+
     }
 }
