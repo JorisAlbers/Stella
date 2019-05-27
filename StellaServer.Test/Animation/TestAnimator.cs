820b11d2094314b48067b266e4b4cf0701b23341
JorisAlbers <joris_albers@hotmail.com>
2019-05-27 21:34:26 +0200

    Added TestAnimator

---
+++ StellaServer.Test/Animation/TestAnimator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Moq;
+using NUnit.Framework;
+using StellaLib.Animation;
+using StellaServer.Animation;
+using StellaServer.Animation.Drawing;
+using StellaServer.Animation.Mapping;
+
+namespace StellaServer.Test.Animation
+{
+    [TestFixture]
+    public class TestAnimator
+    {
+        [Test]
+        public void GetNextFramePerPi_ThreePiMappings_ReturnsFrameForEachPi()
+        {
+            Color expectedColor1 = Color.FromArgb(1, 2, 3);
+            Color expectedColor2 = Color.FromArgb(4, 5, 6);
+            Color expectedColor3 = Color.FromArgb(7, 8, 9);
+
+            int piIndex1 = 0;
+            int piIndex2 = 1;
+            int piIndex3 = 2;
+
+            int expectedPixelIndex1 = 20;
+            int expectedPixelIndex2 = 30;
+            int expectedPixelIndex3 = 40;
+
+            int expectedFrameIndex = 1;
+            int expectedRelativeTimeStamp = 10;
+
+
+            List<Frame> frames = new List<Frame>
+            {
+                new Frame(expectedFrameIndex,expectedRelativeTimeStamp)
+                {
+                    new PixelInstruction(0,expectedColor1),
+                    new PixelInstruction(1,expectedColor2),
+                    new PixelInstruction(2,expectedColor3),
+                }
+            };
+
+            var drawerMock = new Mock<IDrawer>();
+            drawerMock.Setup(x => x.GetEnumerator()).Returns(frames.GetEnumerator());
+            List<PiMaskItem> mask = new List<PiMaskItem>
+            {
+                new PiMaskItem(piIndex1 , expectedPixelIndex1),
+                new PiMaskItem(piIndex2 , expectedPixelIndex2),
+                new PiMaskItem(piIndex3 , expectedPixelIndex3),
+            };
+
+            Animator animator = new Animator(drawerMock.Object,mask,DateTime.Now);
+            Frame[] framePerPi = animator.GetNextFramePerPi();
+
+            Assert.AreEqual(3,framePerPi.Length);
+            // Pi1
+            Frame frame1 = framePerPi[0];
+            Assert.AreEqual(expectedFrameIndex,frame1.Index);
+            Assert.AreEqual(expectedRelativeTimeStamp,frame1.TimeStampRelative);
+            Assert.AreEqual(expectedPixelIndex1,frame1[0].Index);
+            Assert.AreEqual(expectedColor1,frame1[0].Color);
+            // Pi1
+            Frame frame2 = framePerPi[1];
+            Assert.AreEqual(expectedFrameIndex, frame2.Index);
+            Assert.AreEqual(expectedRelativeTimeStamp, frame2.TimeStampRelative);
+            Assert.AreEqual(expectedPixelIndex2, frame2[0].Index);
+            Assert.AreEqual(expectedColor2, frame2[0].Color);
+            // Pi 3
+            Frame frame3 = framePerPi[2];
+            Assert.AreEqual(expectedFrameIndex, frame3.Index);
+            Assert.AreEqual(expectedRelativeTimeStamp, frame3.TimeStampRelative);
+            Assert.AreEqual(expectedPixelIndex3, frame3[0].Index);
+            Assert.AreEqual(expectedColor3, frame3[0].Color);
+
+        }
+
+    }
+}
