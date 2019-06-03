9e139ebcc594ee03a86d30fbe27d1e28b3c054fe
JorisAlbers <joris_albers@hotmail.com>
2019-06-03 21:14:28 +0200

    Added Load_TwoMappings_CorrectlyLoads

--- StellaServer.Test/Serialization/Mapping/TestMappingLoader.cs
+++ StellaServer.Test/Serialization/Mapping/TestMappingLoader.cs
@@ -43,5 +43,51 @@ namespace StellaServer.Test.Serialization.Mapping
 
         }
 
+        [Test]
+        public void Load_TwoMappings_CorrectlyLoads()
+        {
+            int expectedPiIndex1 = 1;
+            int expectedPiIndex2 = 2;
+            int expectedLength1 = 11;
+            int expectedLength2 = 22;
+            int expectedStartIndexOnPi1 = 111;
+            int expectedStartIndexOnPi2 = 222;
+            bool expectedFirstSectionIsInverted1 = true;
+            bool expectedFirstSectionIsInverted2 = false;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("!MappingSettings");
+            stringBuilder.AppendLine("Mappings:");
+            stringBuilder.AppendLine($"  - PiIndex: {expectedPiIndex1}");
+            stringBuilder.AppendLine($"    Length:  {expectedLength1}");
+            stringBuilder.AppendLine($"    StartIndexOnPi:  {expectedStartIndexOnPi1}");
+            stringBuilder.AppendLine($"    FirstSectionIsInverted:  {expectedFirstSectionIsInverted1}");
+            stringBuilder.AppendLine($"  - PiIndex: {expectedPiIndex2}");
+            stringBuilder.AppendLine($"    Length:  {expectedLength2}");
+            stringBuilder.AppendLine($"    StartIndexOnPi:  {expectedStartIndexOnPi2}");
+            stringBuilder.AppendLine($"    FirstSectionIsInverted:  {expectedFirstSectionIsInverted2}");
+
+            MappingLoader loader = new MappingLoader();
+
+            StreamReader mockStream = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(stringBuilder.ToString())));
+
+            List<PiMapping> mappings = loader.Load(mockStream);
+
+            Assert.AreEqual(2, mappings.Count);
+            // Mapping 1
+            PiMapping mapping1 = mappings[0];
+            Assert.AreEqual(expectedPiIndex1, mapping1.PiIndex);
+            Assert.AreEqual(expectedLength1, mapping1.Length);
+            Assert.AreEqual(expectedStartIndexOnPi1, mapping1.StartIndexOnPi);
+            Assert.AreEqual(expectedFirstSectionIsInverted1, mapping1.FirstSectionIsInverted);
+            // Mapping 2
+            PiMapping mapping2 = mappings[1];
+            Assert.AreEqual(expectedPiIndex2, mapping2.PiIndex);
+            Assert.AreEqual(expectedLength2, mapping2.Length);
+            Assert.AreEqual(expectedStartIndexOnPi2, mapping2.StartIndexOnPi);
+            Assert.AreEqual(expectedFirstSectionIsInverted2, mapping2.FirstSectionIsInverted);
+
+        }
+
     }
 }
