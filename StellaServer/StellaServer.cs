9aa67fd4e398ff8ea328e0aac8145e6df9f99355
JorisAlbers <joris_albers@hotmail.com>
2019-06-06 20:49:11 +0200

    Added LoadMask

--- StellaServer/StellaServer.cs
+++ StellaServer/StellaServer.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using StellaServer.Animation.Mapping;
+using StellaServer.Serialization;
+using StellaServer.Serialization.Mapping;
 
 namespace StellaServer
 {
     public class StellaServer
     {
         private readonly string _mappingFilePath;
+        private List<PiMaskItem> _mask;
+
 
         public StellaServer(string mappingFilePath)
         {
@@ -15,7 +21,26 @@ namespace StellaServer
 
         public void Start()
         {
+            // Read mapping
+            try
+            {
+                _mask = LoadMask(_mappingFilePath);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Failed to load mask.",e);
+            }
+        }
 
+        private List<PiMaskItem> LoadMask(string mappingFilePath)
+        {
+            // Read the piMappings from file
+            MappingLoader mappingLoader = new MappingLoader();
+            List<PiMapping> piMappings = mappingLoader.Load(new StreamReader(mappingFilePath));
+
+            // Convert them to a mask
+            PiMaskCalculator piMaskCalculator = new PiMaskCalculator(piMappings);
+            return piMaskCalculator.Calculate();
         }
     }
 }
