3959e55411e0fda7d1090d439088e3a7f8a0220a
JorisAlbers <joris_albers@hotmail.com>
2019-06-10 14:51:40 +0200

    Added APIServer. Right now this is a clone of the Server from StellaServerLib, but this has to be converted to a custom one with separate MessageTypes.

---
+++ StellaServerAPI/APIServer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using StellaLib.Network;
+
+namespace StellaServerAPI
+{
+    /// <summary>
+    /// Connection between this program and API users.
+    /// </summary>
+    public class APIServer
+    {
+        private readonly string _ip;
+        private readonly int _port;
+        private bool _isShuttingDown = false;
+        private object _isShuttingDownLock = new object();
+
+        private ISocketConnection _listenerSocket;
+
+        public List<Client> ConnectedClients { get; set; }
+        
+        public APIServer(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        
+
+        public void Start()
+        {
+            ConnectedClients = new List<Client>();
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(_ip), _port);
+
+            Console.Out.WriteLine($"Starting APIServer on {_port}");
+            // Create a TCP/IP socket.  
+            _listenerSocket = new SocketConnection(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp); // TODO inject with ISocketConnection
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+
+            _listenerSocket.Bind(localEndPoint);
+            _listenerSocket.Listen(100);
+
+            // Start an asynchronous socket to listen for connections.  
+            _listenerSocket.BeginAccept(new AsyncCallback(AcceptCallback), _listenerSocket);
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Console.Out.WriteLine("Received accept callback");
+            // Get the socket that handles the client request.  
+            ISocketConnection listener = (ISocketConnection)ar.AsyncState;
+
+            lock (_isShuttingDownLock)
+            {
+                if (_isShuttingDown)
+                {
+                    Console.WriteLine("Ignored new client callback as the server is shutting down.");
+                    return;
+                }
+            }
+
+            // Start an asynchronous socket to listen for connections.  
+            _listenerSocket.BeginAccept(new AsyncCallback(AcceptCallback), _listenerSocket);
+
+            // Handle the new connection
+            ISocketConnection handler = listener.EndAccept(ar);
+
+            // Create a new client.
+            Client client = new Client(new SocketConnectionController(handler));
+            client.MessageReceived += Client_MessageReceived;
+            client.Disconnect += Client_Disconnected;
+
+            ConnectedClients.Add(client);
+            client.Start();
+        }
+
+        private void Client_Disconnected(object sender, SocketException e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Client_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            lock (_isShuttingDownLock)
+            {
+                _isShuttingDown = true;
+            }
+
+            foreach (Client client in ConnectedClients)
+            {
+                DisposeClient(client);
+            }
+
+           try
+            {
+                _listenerSocket.Shutdown(SocketShutdown.Receive);
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Listening socket was already disconnected.");
+            }
+            _listenerSocket.Dispose();
+            _listenerSocket.Close();
+        }
+
+        private void DisposeClient(Client client)
+        {
+            client.MessageReceived -= Client_MessageReceived;
+            client.Disconnect -= Client_Disconnected;
+            client.Dispose();
+        }
+    }
+}
---
+++ StellaServerAPI/Client.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Net.Sockets;
+using StellaLib.Network;
+
+namespace StellaServerAPI
+{
+    public class Client : IDisposable
+    {
+        private SocketConnectionController SocketConnectionController {get;set;}
+        public int ID { get; set; } = -1;
+        public event EventHandler<SocketException> Disconnect;
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+
+        public Client(SocketConnectionController socketConnectionController)
+        {
+            SocketConnectionController = socketConnectionController;
+            SocketConnectionController.Disconnect += OnDisconnect;
+            SocketConnectionController.MessageReceived += OnMessageReceived;
+        }
+
+        public void Start()
+        {
+            SocketConnectionController.Start();
+        }
+
+        public void Send(MessageType type, byte[] message)
+        {
+            SocketConnectionController.Send(type,message);
+        }
+
+        protected virtual void OnMessageReceived(object sender, MessageReceivedEventArgs eventArgs)
+        {
+            // Bubble the event. Add reference to this object.
+            EventHandler<MessageReceivedEventArgs> handler = MessageReceived;
+            if (handler != null)
+            {
+                handler(this, eventArgs);
+            }
+        }
+
+        protected virtual void OnDisconnect(object sender, SocketException exception)
+        {
+            // Bubble the event. Add reference to this object.
+            EventHandler<SocketException> handler = Disconnect;
+            if (handler != null)
+            {
+                handler(this, exception);
+            }
+        }
+
+        public void Dispose()
+        {
+            SocketConnectionController.MessageReceived -= OnMessageReceived;
+            SocketConnectionController.Disconnect -= OnDisconnect;
+            SocketConnectionController.Dispose();
+        }
+    }
+}
