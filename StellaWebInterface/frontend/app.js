be44d001a154ed821d321eddc3c6c841efeb7c3f
Martijn van der Heiden <martijn.van.der.heiden@dpdk.com>
2019-06-10 15:14:11 +0200

    Initial StellaWebInterface commit

---
+++ StellaWebInterface/frontend/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {MuiThemeProvider} from '@material-ui/core/styles/index';
+import AppRouter from './routers/AppRouter';
+import './styles/styles.css';
+import {theme} from './theme/theme';
+
+const App = () => (
+    <MuiThemeProvider theme={theme}>
+      <AppRouter/>
+    </MuiThemeProvider>
+);
+
+render(<App/>, document.getElementById('app'));
