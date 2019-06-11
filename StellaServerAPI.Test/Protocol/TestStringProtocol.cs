1d6514a2bbe0314c651675d1967ee16053b4a185
JorisAlbers <joris_albers@hotmail.com>
2019-06-11 21:32:02 +0200

    Added StringProtocol

---
+++ StellaServerAPI.Test/Protocol/TestStringProtocol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using StellaServerAPI.Protocol;
+
+namespace StellaServerAPI.Test.Protocol
+{
+    [TestFixture]
+    public class TestStringProtocol
+    {
+        [Test]
+        public void Deserialize_SmallString_ReturnsASinglePackage()
+        {
+            string message = "ThisIsAMessage";
+            byte[] expectedBytes = Encoding.ASCII.GetBytes(message);
+            int expectedNumberOfPackages = 1;
+            int expectedPackageIndex = 0;
+
+            byte[][] packages = StringProtocol.Serialize(message);
+
+            Assert.AreEqual(1,packages.Length);
+            // number of packages
+            Assert.AreEqual(expectedNumberOfPackages,BitConverter.ToInt32(packages[0],0));
+            Assert.AreEqual(expectedPackageIndex,BitConverter.ToInt32(packages[0],4));
+
+            byte[] messageBuffer = new byte[expectedBytes.Length];
+            Array.Copy(packages[0],8, messageBuffer,0, expectedBytes.Length);
+            Assert.AreEqual(expectedBytes, messageBuffer);
+        }
+    }
+}
---
+++ StellaServerAPI/Protocol/StringProtocol.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using StellaLib.Network.Protocol;
+
+namespace StellaServerAPI.Protocol
+{
+    public static class StringProtocol
+    {
+        const int FIRST_PACKAGE_HEADER_BYTES = sizeof(int); // numberOfPackages
+        const int PACKAGE_HEADER_BYTES = sizeof(int); // PackageIndex
+
+        // Convert the string to an array of byte arrays
+        public static byte[][] Serialize(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            if (data.Length < PacketProtocol<MessageType>.BUFFER_SIZE -  FIRST_PACKAGE_HEADER_BYTES + PACKAGE_HEADER_BYTES)
+            {
+                // just one package needed
+                byte[] buffer = new byte[data.Length + FIRST_PACKAGE_HEADER_BYTES + PACKAGE_HEADER_BYTES];
+                BitConverter.GetBytes(1).CopyTo(buffer,0);
+                BitConverter.GetBytes(0).CopyTo(buffer,4);
+                data.CopyTo(buffer,8);
+                return new byte[1][]{buffer};
+            }
+            
+            // Multiple packages are needed to be able to send the message.
+
+            throw new NotImplementedException();
+
+
+        }
+
+
+
+    }
+}
