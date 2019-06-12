a30b25c1ee150c8055f7227a8cf2d55711915852
JorisAlbers <joris_albers@hotmail.com>
2019-06-12 21:23:38 +0200

    Implemented the return of multiple packages when the string is too big to send in one package

--- StellaServerAPI.Test/Protocol/TestStringProtocol.cs
+++ StellaServerAPI.Test/Protocol/TestStringProtocol.cs
@@ -11,21 +11,56 @@ namespace StellaServerAPI.Test.Protocol
         [Test]
         public void Deserialize_SmallString_ReturnsASinglePackage()
         {
+            int maxPackageSize = 300;
             string message = "ThisIsAMessage";
             byte[] expectedBytes = Encoding.ASCII.GetBytes(message);
             int expectedNumberOfPackages = 1;
-            int expectedPackageIndex = 0;
 
-            byte[][] packages = StringProtocol.Serialize(message);
+            byte[][] packages = StringProtocol.Serialize(message,maxPackageSize);
 
             Assert.AreEqual(1,packages.Length);
             // number of packages
             Assert.AreEqual(expectedNumberOfPackages,BitConverter.ToInt32(packages[0],0));
-            Assert.AreEqual(expectedPackageIndex,BitConverter.ToInt32(packages[0],4));
 
             byte[] messageBuffer = new byte[expectedBytes.Length];
-            Array.Copy(packages[0],8, messageBuffer,0, expectedBytes.Length);
+            Array.Copy(packages[0],4, messageBuffer,0, expectedBytes.Length);
             Assert.AreEqual(expectedBytes, messageBuffer);
+        }
+
+        [Test]
+        public void Deserialize_LargeString_ReturnsTwoPackages()
+        {
+            int maxPackageSize = 40;
+            string message = "ThisIsALargeMessageThatDoesNotFitInASinglePackage";
+            byte[] expectedBytes = Encoding.ASCII.GetBytes(message); // 49 bytes
+            byte[] expectedStringData1 = new byte[36]; // 40 (max package size) -4 (header of the package)
+            Array.Copy(expectedBytes, 0,expectedStringData1,0, expectedStringData1.Length);
+            byte[] expectedStringData2 = new byte[13]; //  13 (49 - 36)
+            Array.Copy(expectedBytes, 36, expectedStringData2, 0, expectedStringData2.Length);
+            
+            int expectedNumberOfPackages = 2;
+            int expectedPackageIndex = 1;
+
+            byte[][] packages = StringProtocol.Serialize(message, maxPackageSize);
+
+            Assert.AreEqual(2, packages.Length);
+            // First package
+            // number of packages
+            byte[] package = packages[0];
+            Assert.AreEqual(expectedNumberOfPackages, BitConverter.ToInt32(package, 0));
+
+            byte[] messageBuffer = new byte[expectedStringData1.Length];
+            Array.Copy(package, 4, messageBuffer, 0, expectedStringData1.Length);
+            Assert.AreEqual(expectedStringData1, messageBuffer);
+
+            // Second package
+            package = packages[1];
+            Assert.AreEqual(expectedPackageIndex, BitConverter.ToInt32(package, 0));
+
+            messageBuffer = new byte[expectedStringData2.Length];
+            Array.Copy(package, 4, messageBuffer, 0, expectedStringData2.Length);
+            Assert.AreEqual(expectedStringData2, messageBuffer);
+
         }
     }
 }
--- StellaServerAPI/Protocol/StringProtocol.cs
+++ StellaServerAPI/Protocol/StringProtocol.cs
@@ -6,31 +6,41 @@ namespace StellaServerAPI.Protocol
 {
     public static class StringProtocol
     {
-        const int FIRST_PACKAGE_HEADER_BYTES = sizeof(int); // numberOfPackages
-        const int PACKAGE_HEADER_BYTES = sizeof(int); // PackageIndex
+        /// <summary>
+        /// The number of bytes needed for the header of the package.
+        /// Represents:
+        /// - Number of packages if it's the first package.
+        /// - Package index if it's an subsequent package.
+        /// </summary>
+        const int PACKAGE_HEADER_BYTES = sizeof(int);
 
         // Convert the string to an array of byte arrays
-        public static byte[][] Serialize(string message)
+        public static byte[][] Serialize(string message, int maxPackageSize)
         {
             byte[] data = Encoding.ASCII.GetBytes(message);
-            if (data.Length < PacketProtocol<MessageType>.BUFFER_SIZE -  FIRST_PACKAGE_HEADER_BYTES + PACKAGE_HEADER_BYTES)
-            {
-                // just one package needed
-                byte[] buffer = new byte[data.Length + FIRST_PACKAGE_HEADER_BYTES + PACKAGE_HEADER_BYTES];
-                BitConverter.GetBytes(1).CopyTo(buffer,0);
-                BitConverter.GetBytes(0).CopyTo(buffer,4);
-                data.CopyTo(buffer,8);
-                return new byte[1][]{buffer};
-            }
+
+            int bytesAvailablePerPackage = maxPackageSize - PACKAGE_HEADER_BYTES;
+            int packagesNeeded = (data.Length + bytesAvailablePerPackage - 1) / bytesAvailablePerPackage;
             
-            // Multiple packages are needed to be able to send the message.
-
-            throw new NotImplementedException();
-
-
+            byte[][] returnData = new byte[packagesNeeded][];
+            // Create first package
+            returnData[0] = CreatePackage(data, 0, maxPackageSize, packagesNeeded);
+            for (int i = 1; i < packagesNeeded; i++)
+            {
+                returnData[i] = CreatePackage(data, i * bytesAvailablePerPackage, maxPackageSize, i);
+            }
+            return returnData;
         }
 
-
-
+        /// <summary>
+        ///  Create a subsequent package with a normal package header
+        /// </summary>
+        private static byte[] CreatePackage(byte[] data, int startIndex, int maxPackageSize, int headerCounter)
+        {
+            byte[] buffer = new byte[Math.Min(data.Length - startIndex + PACKAGE_HEADER_BYTES, maxPackageSize + PACKAGE_HEADER_BYTES)];
+            BitConverter.GetBytes(headerCounter).CopyTo(buffer, 0);
+            Array.Copy(data,startIndex,buffer,4,buffer.Length - 4);
+            return buffer;
+        }
     }
 }
