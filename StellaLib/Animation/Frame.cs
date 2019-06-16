acfeeaaf4dff4bddc2562e62b6185e5456639d12
JorisAlbers <joris_albers@hotmail.com>
2019-06-16 21:11:51 +0200

    Added FrameWithoutDelta

--- StellaLib/Animation/Frame.cs
+++ StellaLib/Animation/Frame.cs
@@ -51,4 +51,52 @@ namespace StellaLib.Animation
             return true;
         }
     }
+
+    /// <summary>
+    /// Frame with pixelInstructions without a delta, so no indexes.
+    /// A frame contains:
+    ///     1. All pixel instructions of a single moment in time.
+    ///     2. The display time 
+    /// 
+    /// </summary>
+    [DebuggerDisplay("Index = {Index}, TSR = {TimeStampRelative}, Count = {Count}")]
+    public class FrameWithoutDelta : List<PixelInstructionWithoutDelta>, IEquatable<Frame>
+    {
+        /// <summary>
+        /// The index of the Frame in the frameSet.
+        /// </summary>
+        public int Index;
+
+        /// <summary>
+        /// The number of milliseconds after the start of the FrameSet (animation) this frame will be shown.
+        /// </summary>
+        /// <value></value>
+        public int TimeStampRelative { get; set; }
+
+
+        [DebuggerStepThrough]
+        public FrameWithoutDelta(int index, int timeStampRelative, int capacity) : base(capacity)
+        {
+            Index = index;
+            TimeStampRelative = timeStampRelative;
+        }
+
+        public bool Equals(Frame other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (Index != other.Index) return false;
+            if (TimeStampRelative != other.TimeStampRelative) return false;
+            if (Count != other.Count) return false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (!this[i].Equals(other[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
