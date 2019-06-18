c5044b6499573057e1b57b2ae5fbb190df7ea177
JorisAlbers <joris_albers@hotmail.com>
2019-06-18 20:03:17 +0200

    Added RepeatingPatternsDrawerWithoutDelta

--- StellaServerLib.Test/Animation/Drawing/TestRepeatingPatternsDrawer.cs
+++ StellaServerLib.Test/Animation/Drawing/TestRepeatingPatternsDrawer.cs
@@ -110,4 +110,71 @@ namespace StellaServerLib.Test.Animation.Drawing
             Assert.AreEqual(frame2[0].Color, expectedColor2);
         }
     }
+
+    [TestFixture]
+    public class TestRepeatingPatternsDrawerWithoutDelta
+    {
+        [Test]
+        public void Create_singlePattern_CreatesCorrectFrames()
+        {
+            // Setup
+            Color[] pattern = new Color[]{
+                 Color.FromArgb(1,2,3),
+                 Color.FromArgb(4,5,6),
+                 Color.FromArgb(7,8,9)
+                 };
+            int lengthStrip = 7;
+            int frameWaitMS = 100;
+            RepeatingPatternsDrawerWithoutDelta drawer = new RepeatingPatternsDrawerWithoutDelta(lengthStrip, frameWaitMS, new Color[][] { pattern });
+
+            // Expected
+            Color expectedColor1 = Color.FromArgb(1, 2, 3);
+            Color expectedColor2 = Color.FromArgb(4, 5, 6);
+            Color expectedColor3 = Color.FromArgb(7, 8, 9);
+
+            FrameWithoutDelta frame = drawer.First();
+
+            //Assert
+            Assert.AreEqual(lengthStrip, frame.Count);
+            Assert.AreEqual(0, frame.TimeStampRelative);
+            Assert.AreEqual(frame[0].Color, expectedColor1);
+            Assert.AreEqual(frame[1].Color, expectedColor2);
+            Assert.AreEqual(frame[2].Color, expectedColor3);
+            Assert.AreEqual(frame[3].Color, expectedColor1);
+            Assert.AreEqual(frame[4].Color, expectedColor2);
+            Assert.AreEqual(frame[5].Color, expectedColor3);
+            Assert.AreEqual(frame[6].Color, expectedColor1);
+        }
+
+        [Test]
+        public void Create_TwoPatterns_CreatesFrameListWithTwoFrames()
+        {
+            // Setup
+            Color[][] patterns = new Color[][]
+            {
+                new Color[] {Color.FromArgb(1, 2, 3)},
+                new Color[] {Color.FromArgb(4, 5, 6)}
+
+            };
+
+            int lengthStrip = 7;
+            int frameWaitMS = 100;
+            RepeatingPatternsDrawerWithoutDelta drawer = new RepeatingPatternsDrawerWithoutDelta(lengthStrip, frameWaitMS, patterns);
+
+            // Expected
+            Color expectedColor1 = Color.FromArgb(1, 2, 3);
+            Color expectedColor2 = Color.FromArgb(4, 5, 6);
+
+            List<FrameWithoutDelta> frames = drawer.Take(2).ToList();
+
+
+            //Assert
+            FrameWithoutDelta frame1 = frames[0];
+            Assert.AreEqual(0, frame1.TimeStampRelative);
+            Assert.AreEqual(frame1[0].Color, expectedColor1);
+            FrameWithoutDelta frame2 = frames[1];
+            Assert.AreEqual(frameWaitMS, frame2.TimeStampRelative);
+            Assert.AreEqual(frame2[0].Color, expectedColor2);
+        }
+    }
 }
--- StellaServerLib/Animation/Drawing/RepeatingPatternsDrawer.cs
+++ StellaServerLib/Animation/Drawing/RepeatingPatternsDrawer.cs
@@ -78,4 +78,68 @@ namespace StellaServerLib.Animation.Drawing
             return GetEnumerator();
         }
     }
+
+    /// <summary>
+    /// Repeats one or multiple pattern(s) over the length of the ledstip.
+    /// Each frame is a Color[] pattern repeeated over the ledstrip.
+    /// </summary>
+    public class RepeatingPatternsDrawerWithoutDelta : IDrawerWithoutDelta
+    {
+        private int _lengthStrip;
+        private int _frameWaitMS;
+        private Color[][] _patterns;
+
+        /// <summary>
+        /// Each array of colors in the patterns list will be repeated over the length of the ledstip.
+        /// Each frame is the next pattern, repeated.
+        /// </summary>
+        /// <param name="patterns"></param>
+        public RepeatingPatternsDrawerWithoutDelta(int lengthStrip, int frameWaitMS, Color[][] patterns)
+        {
+            _lengthStrip = lengthStrip;
+            _patterns = patterns;
+            _frameWaitMS = frameWaitMS;
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<FrameWithoutDelta> GetEnumerator()
+        {
+            int relativeTimeStamp = 0;
+            int frameIndex = 0;
+            while (true)
+            {
+                foreach (Color[] pattern in _patterns)
+                {
+                    int patternsInStrip = _lengthStrip / pattern.Length;
+                    FrameWithoutDelta frame = new FrameWithoutDelta(frameIndex++, relativeTimeStamp, _lengthStrip);
+                    int leftPixelIndex = 0;
+                    for (int j = 0; j < patternsInStrip; j++)
+                    {
+                        leftPixelIndex = pattern.Length * j;
+                        for (int k = 0; k < pattern.Length; k++)
+                        {
+                            int pixelIndex = leftPixelIndex + k;
+                            frame[pixelIndex] = new PixelInstructionWithoutDelta(pattern[k]);
+                        }
+                    }
+
+                    leftPixelIndex = leftPixelIndex + pattern.Length;
+                    // draw remaining pixels of the pattern that does not completely fit on the end of the led strip
+                    for (int j = 0; j < _lengthStrip % pattern.Length; j++)
+                    {
+                        int pixelIndex = leftPixelIndex + j;
+                        frame[pixelIndex] = new PixelInstructionWithoutDelta(pattern[j]);
+                    }
+
+                    yield return frame;
+                    relativeTimeStamp += _frameWaitMS;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
