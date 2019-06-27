4d11702195738050e155dc418d82d4db3bc0a967
JorisAlbers <joris_albers@hotmail.com>
2019-06-27 21:44:15 +0200

    Added TestPackageProtocol

---
+++ StellaLib.Test/Network/Protocol/TestPackageProtocol.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using StellaLib.Network;
+using StellaLib.Network.Protocol;
+
+namespace StellaLib.Test.Network.Protocol
+{
+    [TestFixture]
+    public class TestPackageProtocol
+    {
+        [Test]
+        public void DataReceived_FullMessage_ReturnsCorrectMessage()
+        {
+            MessageType expectedMessageType = MessageType.Init;
+            byte[] expectedBytes = new byte[] {1, 2, 3, 4};
+
+            byte[] message = PacketProtocol<MessageType>.WrapMessage(expectedMessageType, expectedBytes);
+
+            MessageType receivedMessageType = MessageType.Unknown;
+            byte[] receivedBytes = null;
+
+            PacketProtocol<MessageType> packetProtocol = new PacketProtocol<MessageType>();
+            packetProtocol.MessageArrived += (type, bytes) =>
+            {
+                receivedMessageType = type;
+                receivedBytes = bytes;
+            };
+
+            packetProtocol.DataReceived(message);
+
+            Assert.AreEqual(expectedMessageType, receivedMessageType);
+            Assert.AreEqual(expectedBytes, receivedBytes);
+        }
+    }
+}
