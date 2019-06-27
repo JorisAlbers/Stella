35f1eb2cf4ded68cb6bee691f8d87a9338dd1ec9
JorisAlbers <joris_albers@hotmail.com>
2019-06-27 21:56:41 +0200

    Added test with a message send using multiple packages

--- StellaLib.Test/Network/Protocol/TestPackageProtocol.cs
+++ StellaLib.Test/Network/Protocol/TestPackageProtocol.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using StellaLib.Network;
 using StellaLib.Network.Protocol;
 
@@ -30,5 +31,53 @@ namespace StellaLib.Test.Network.Protocol
             Assert.AreEqual(expectedMessageType, receivedMessageType);
             Assert.AreEqual(expectedBytes, receivedBytes);
         }
+
+        [Test]
+        public void DataReceived_MessageSplitInMultiplePackages_ReturnsCorrectMessage()
+        {
+            MessageType expectedMessageType = MessageType.Init;
+            byte[] expectedBytes = new byte[] { 1, 2, 3, 4 };
+
+            byte[] message = PacketProtocol<MessageType>.WrapMessage(expectedMessageType, expectedBytes);
+
+            Assert.AreEqual(12, message.Length, "This test assumes the message length is 12");
+
+            byte[] messagePackage1 = {message[0],
+                                      message[1],
+                                      message[2]};
+            byte[] messagePackage2 = {message[3],
+                                      message[4],
+                                      message[5]};
+            byte[] messagePackage3 = {message[6],
+                                      message[7],
+                                      message[8]};
+            byte[] messagePackage4 = {message[9],
+                                      message[10],
+                                      message[11],
+            };
+
+            MessageType receivedMessageType = MessageType.Unknown;
+            byte[] receivedBytes = null;
+
+            PacketProtocol<MessageType> packetProtocol = new PacketProtocol<MessageType>();
+            packetProtocol.MessageArrived += (type, bytes) =>
+            {
+                receivedMessageType = type;
+                receivedBytes = bytes;
+            };
+
+            packetProtocol.DataReceived(messagePackage1);
+            Assert.AreEqual(MessageType.Unknown, receivedMessageType);
+            packetProtocol.DataReceived(messagePackage2);
+            Assert.AreEqual(MessageType.Unknown, receivedMessageType);
+            packetProtocol.DataReceived(messagePackage3);
+            Assert.AreEqual(MessageType.Unknown, receivedMessageType);
+
+            packetProtocol.DataReceived(messagePackage4);
+
+            Assert.AreEqual(expectedMessageType, receivedMessageType);
+            Assert.AreEqual(expectedBytes, receivedBytes);
+        }
+
     }
 }
