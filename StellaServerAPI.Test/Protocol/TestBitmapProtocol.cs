fec32532d0122b928f2ee8200a12fd3db604e16e
JorisAlbers <joris_albers@hotmail.com>
2019-07-06 15:15:10 +0200

    Added BitmapProtocol

---
+++ StellaServerAPI.Test/Protocol/TestBitmapProtocol.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using NUnit.Framework;
+using StellaServerAPI.Protocol;
+
+namespace StellaServerAPI.Test.Protocol
+{
+    [TestFixture]
+    public class TestBitmapProtocol
+    {
+        [Test]
+        public void TryDeserialize_byteArray_ReturnsBitmap()
+        {
+            Color[] expectedColors1 = new Color[]{Color.FromArgb(255,255,255), Color.FromArgb(200,200,200)};
+            Color[] expectedColors2 = new Color[]{Color.FromArgb(150,150,150), Color.FromArgb(0,0,0)};
+            int width = 2;
+            int height = 2;
+
+            byte[] data = new byte[4 + 4 + 4 * 3]; // width + height + 4 * rgb
+            int startIndex = 0;
+            BitConverter.GetBytes(width) .CopyTo(data, startIndex);
+            BitConverter.GetBytes(height).CopyTo(data, startIndex +=4);
+            // Row 1
+            data[startIndex += 4] = expectedColors1[0].R;
+            data[startIndex += 1] = expectedColors1[0].G;
+            data[startIndex += 1] = expectedColors1[0].B;
+            data[startIndex += 1] = expectedColors1[1].R;
+            data[startIndex += 1] = expectedColors1[1].G;
+            data[startIndex += 1] = expectedColors1[1].B;
+            // Row 2
+            data[startIndex += 1] = expectedColors2[0].R;
+            data[startIndex += 1] = expectedColors2[0].G;
+            data[startIndex += 1] = expectedColors2[0].B;
+            data[startIndex += 1] = expectedColors2[1].R;
+            data[startIndex += 1] = expectedColors2[1].G;
+            data[startIndex += 1] = expectedColors2[1].B;
+            
+            BitmapProtocol bitmapProtocol = new BitmapProtocol();
+            bool received = bitmapProtocol.TryDeserialize(data, out Bitmap bitmap);
+
+            Assert.IsTrue(received);
+            Assert.AreEqual(width, bitmap.Width);
+            Assert.AreEqual(height, bitmap.Height);
+            Assert.AreEqual(expectedColors1[0], bitmap.GetPixel(0,0));
+            Assert.AreEqual(expectedColors1[1], bitmap.GetPixel(1,0));
+            Assert.AreEqual(expectedColors2[0], bitmap.GetPixel(0,1));
+            Assert.AreEqual(expectedColors2[1], bitmap.GetPixel(1,1));
+
+
+        }
+    }
+}
---
+++ StellaServerAPI/Protocol/BitmapProtocol.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+
+namespace StellaServerAPI.Protocol
+{
+    public class BitmapProtocol
+    {
+        private int _pixelsReceived;
+        private int _rowsReceived;
+        private Bitmap _bitmap;
+
+
+        public BitmapProtocol()
+        {
+            _pixelsReceived = 0;
+            _rowsReceived = 0;
+        }
+
+        /// <summary>
+        /// True if all data has been received
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public bool TryDeserialize(byte[] buffer, out Bitmap bitmap)
+        {
+            int bufferStartIndex = 0;
+            if (_bitmap == null)
+            {
+                // This is the first package
+                int numberOfPixels = BitConverter.ToInt32(buffer, 0);
+                int numberOfRows = BitConverter.ToInt32(buffer, 4);
+                bufferStartIndex = 8;
+
+                _bitmap = new Bitmap(numberOfPixels, numberOfRows);
+            }
+
+            // From this point on, the buffer contains rgb values for each pixel. 
+            // So [R, G, B, R, G, B, ...]
+            for (int i = bufferStartIndex; i < buffer.Length; i+= 3)
+            {
+                byte red   = buffer[i];
+                byte green = buffer[i+1];
+                byte blue  = buffer[i+2];
+
+                _bitmap.SetPixel(_pixelsReceived, _rowsReceived, Color.FromArgb(red,green,blue));
+
+                if (++_pixelsReceived == _bitmap.Width)
+                {
+                    _pixelsReceived = 0;
+                    if (++_rowsReceived == _bitmap.Height)
+                    {
+                        bitmap = _bitmap;
+                        return true;
+                    }
+                }
+            }
+
+            bitmap = null;
+            return false;
+        }
+
+    }
+}
