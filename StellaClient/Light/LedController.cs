30c39f85b634344f03abee2eb5e0edea73306ec5
JorisAlbers <joris_albers@hotmail.com>
2019-07-21 21:18:06 +0200

    Added a minimum frame rate.

--- StellaClient/Light/LedController.cs
+++ StellaClient/Light/LedController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using rpi_ws281x;
 using StellaLib.Animation;
 
@@ -11,20 +12,40 @@ namespace StellaClient.Light
     /// </summary>
     public class LedController : ILedController
     {
+        private const long MINIMUM_TICKS_PER_FRAME = TimeSpan.TicksPerMillisecond * 50;
         private readonly ILEDStrip _ledStrip;
+        private long _nextRenderAllowedAfter;
+        private object lockObject = new object();
 
         public LedController(ILEDStrip ledStrip)
         {
             _ledStrip = ledStrip;
+            _nextRenderAllowedAfter = DateTime.Now.Ticks;
         }
 
         public void RenderFrame(FrameWithoutDelta frame)
         {
-            for (int i = 0; i < frame.Count; i++)
+            if (Monitor.TryEnter(lockObject))
             {
-                _ledStrip.SetLEDColor(0, i, frame[i].Color);
+                try
+                {
+                    DateTime now = DateTime.Now;
+                    if (now.Ticks >= _nextRenderAllowedAfter)
+                    {
+                        for (int i = 0; i < frame.Count; i++)
+                        {
+                            _ledStrip.SetLEDColor(0, i, frame[i].Color);
+                        }
+
+                        _ledStrip.Render();
+                        _nextRenderAllowedAfter = now.Ticks + MINIMUM_TICKS_PER_FRAME;
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lockObject);
+                }
             }
-            _ledStrip.Render();
         }
     }
 }
