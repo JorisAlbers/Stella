948f4362cb62ae278ec2d0ea534a65c6332f1c88
JorisAlbers <joris_albers@hotmail.com>
2019-07-21 11:57:15 +0200

    Added FrameProvider

---
+++ StellaServerLib/Animation/FrameProvider/FrameProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using StellaLib.Animation;
+using StellaServerLib.Animation.Drawing;
+
+namespace StellaServerLib.Animation.FrameProvider
+{
+    /// <summary>
+    /// Provides the next frame to animate.
+    /// </summary>
+    public class FrameProvider : IFrameProvider
+    {
+        private readonly IDrawer _drawer;
+        private readonly AnimationTransformation _animationTransformation;
+
+        public FrameProvider(IDrawer drawer, AnimationTransformation animationTransformation)
+        {
+            _drawer = drawer;
+            _animationTransformation = animationTransformation;
+        }
+
+
+        public IEnumerator<Frame> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
