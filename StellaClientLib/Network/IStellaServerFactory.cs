3f8553eba4316f3bcdd5830ac4cd970f3be2da80
JorisAlbers <joris_albers@hotmail.com>
2019-08-04 14:35:23 +0200

    Added IStellaServerFactory

---
+++ StellaClientLib/Network/IStellaServerFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace StellaClientLib.Network
+{
+    public interface IStellaServerFactory
+    {
+        IStellaServer Create(IPEndPoint serverAdress, int udpPort, int ID);
+    }
+}
