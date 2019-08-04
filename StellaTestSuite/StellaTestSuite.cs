52b613bb88ae1444b1ee22b74196898b05fb8ea6
JorisAlbers <joris_albers@hotmail.com>
2019-08-04 15:01:26 +0200

    Added StellaTestSuite.cs

---
+++ StellaTestSuite/StellaTestSuite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StellaClientLib;
+using StellaClientLib.Serialization;
+using StellaTestSuite.Model.Client;
+
+namespace StellaTestSuite
+{
+    class StellaTestSuite
+    {
+        private StellaClient[] clients;
+
+        public StellaTestSuite(int numberOfClients, int numberOfPixelsPerRow, int numberOfRows, int minimumFrameRate, byte brightness)
+        {
+            clients = new StellaClient[numberOfClients];
+            clients[0] = CreateClient(0,numberOfPixelsPerRow * numberOfRows, minimumFrameRate,brightness);
+            clients[1] = CreateClient(1,numberOfPixelsPerRow * numberOfRows, minimumFrameRate,brightness);
+            clients[2] = CreateClient(2,numberOfPixelsPerRow * numberOfRows, minimumFrameRate,brightness);
+        }
+
+
+        private StellaClient CreateClient(int id, int numberOfPixels, int minimumFrameRate, byte brightness)
+        {
+            Configuration configuration = new Configuration(id,string.Empty,0,0,numberOfPixels,0,0, minimumFrameRate, brightness);
+            return new StellaClient(configuration, new MemoryStellaServerFactory());
+        }
+    }
+}
