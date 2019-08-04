b0c917c02e54933b2fcb7e9fbba96890df129890
JorisAlbers <joris_albers@hotmail.com>
2019-08-04 14:31:09 +0200

    Added MemoryStellaServer

---
+++ StellaTestSuite/Model/Client/MemoryStellaServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StellaClientLib.Network;
+using StellaLib.Animation;
+using StellaLib.Network;
+
+namespace StellaTestSuite.Model.Client
+{
+    public class MemoryStellaServer : IStellaServer
+    {
+        /// <summary>
+        /// Fired when the client is sending a message
+        /// </summary>
+        public event EventHandler<MessageType> MessageSend;
+
+        public void Dispose()
+        {
+            ;
+        }
+
+        public void Send(MessageType type, byte[] message)
+        {
+            var eventHandler = MessageSend;
+            if (eventHandler != null)
+            {
+                eventHandler.Invoke(this,type);
+            }
+        }
+
+        public void Start()
+        {
+            ;
+        }
+
+        public event EventHandler<FrameWithoutDelta> RenderFrameReceived;
+    }
+}
