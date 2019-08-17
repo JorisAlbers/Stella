a66a7dea1c00e6fc0f7e06710bdf603d626e4c4a
JorisAlbers <joris_albers@hotmail.com>
2019-08-17 10:44:31 +0200

    Bugfix: The master animationTransformation is now preserved on the creation of a new animation

--- StellaServerLib/Animation/AnimatorCreation.cs
+++ StellaServerLib/Animation/AnimatorCreation.cs
@@ -35,7 +35,7 @@ namespace StellaServerLib.Animation
                 masterTransformationSettings = new TransformationSettings(0,0,new float[3]);
             }
 
-            TransformationController transformationController = new TransformationController(masterTransformationSettings,animationTransformationSettings);
+            _transformationController = new TransformationController(masterTransformationSettings,animationTransformationSettings);
 
 
             if (storyboard.AnimationSettings.Length == 1)// TODO remove this redundant if statement
@@ -44,7 +44,7 @@ namespace StellaServerLib.Animation
                 IDrawer drawer = CreateDrawer(storyboard.AnimationSettings[0]);
                 animationTransformationSettings[0] = new TransformationSettings(storyboard.AnimationSettings[0].FrameWaitMs,0,new float[3]);
 
-                frameProvider = new FrameProvider(drawer, transformationController);
+                frameProvider = new FrameProvider(drawer, _transformationController);
             }
             else
             {
@@ -78,10 +78,10 @@ namespace StellaServerLib.Animation
                 }
 
                 // Then, create a new FrameProvider with multiple animations
-                frameProvider = new FrameProvider(drawers, relativeTimeStamps, transformationController);
+                frameProvider = new FrameProvider(drawers, relativeTimeStamps, _transformationController);
             }
 
-            return new Animator(frameProvider, stripLengthPerPi, mask, transformationController);
+            return new Animator(frameProvider, stripLengthPerPi, mask, _transformationController);
         }
 
         private IDrawer CreateDrawer(IAnimationSettings animationSetting)
