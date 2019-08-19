44af86fa985c47051e3a6e63ffcae9293c9e3c0c
JorisAlbers <joris_albers@hotmail.com>
2019-08-19 20:50:12 +0200

    Added test to make sure the master transformation setting is preserved

---
+++ StellaServerLib.Test/Animation/TestAnimatorCreator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using NUnit.Framework;
+using StellaServerLib.Animation;
+using StellaServerLib.Animation.Transformation;
+using StellaServerLib.Serialization.Animation;
+
+namespace StellaServerLib.Test.Animation
+{
+    public class TestAnimatorCreator
+    {
+
+        [Test]
+        public void MasterTransformationSettingsIsPreserved()
+        {
+            AnimatorCreation creator = new AnimatorCreation(null);
+
+            MovingPatternAnimationSettings animationSettings = new MovingPatternAnimationSettings
+            {
+                FrameWaitMs = 10,
+                Pattern = new Color[] {Color.Red},
+                RelativeStart = 10,
+                StartIndex = 0,
+                StripLength = 100
+            };
+
+
+            Storyboard sb1 = new Storyboard {AnimationSettings = new IAnimationSettings[] {animationSettings}};
+            Storyboard sb2 = new Storyboard {AnimationSettings = new IAnimationSettings[] {animationSettings}};
+
+            Animator animator = creator.Create(sb1, new int[]{100}, null);
+            TransformationSettings expectedSettings =
+                animator.TransformationController.AnimationTransformation.MasterTransformationSettings;
+
+            animator = creator.Create(sb2, new int[] { 100 }, null);
+            Assert.IsTrue(ReferenceEquals(expectedSettings, animator.TransformationController.AnimationTransformation.MasterTransformationSettings));
+            
+        }
+    }
+}
