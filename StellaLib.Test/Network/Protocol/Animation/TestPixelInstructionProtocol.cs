d5c2cca50c10f87b02bbc8f8c14fd3e5b6bcf208
JorisAlbers <joris_albers@hotmail.com>
2019-08-19 22:48:45 +0200

    Removed PixelInstruction (instruction with index) protocol related code

--- StellaLib.Test/Network/Protocol/Animation/TestPixelInstructionProtocol.cs
+++ StellaLib.Test/Network/Protocol/Animation/TestPixelInstructionProtocol.cs
@@ -6,42 +6,6 @@ using StellaLib.Network.Protocol.Animation;
 namespace StellaLib.Test.Network.Protocol.Animation
 {
     [TestFixture]
-    public class TestPixelInstructionProtocol
-    {
-        [Test]
-        public void Serialize_PixelinStruction_CorrectlyConvertedToByteArray()
-        {
-            PixelInstruction pi = new PixelInstruction(10,1,2,3);
-
-            byte[] expectedBytes = new byte[sizeof(int)+ 1 + 1 + 1]; // Index, red, green , blue 
-            BitConverter.GetBytes(pi.Index).CopyTo(expectedBytes,0);
-            expectedBytes[4] = pi.R;
-            expectedBytes[5] = pi.G;
-            expectedBytes[6] = pi.B;
-           
-            byte[] buffer = new byte[PixelInstructionProtocol.BYTES_NEEDED];
-            PixelInstructionProtocol.Serialize(pi,buffer,0);
-            Assert.AreEqual(expectedBytes, buffer );
-        }
-        
-        [Test]
-        public void Deserialize_BytesArray_CorrectlyDeserializesByteArray()
-        {
-            PixelInstruction expectedPixelInstruction = new PixelInstruction(10,1,2,3);
-
-            byte[] bytes = new byte[sizeof(int)+ 1 + 1 + 1]; // Index, red, green , blue 
-            BitConverter.GetBytes(expectedPixelInstruction.Index).CopyTo(bytes,0);
-            bytes[4] = expectedPixelInstruction.R;
-            bytes[5] = expectedPixelInstruction.G;
-            bytes[6] = expectedPixelInstruction.B;
-
-            PixelInstruction pi = PixelInstructionProtocol.Deserialize(bytes,0);
-            Assert.AreEqual(expectedPixelInstruction,pi);
-        }
-        
-    }
-
-    [TestFixture]
     public class TestPixelInstructionWithoutDeltaProtocol
     {
         [Test]
--- StellaLib/Network/Protocol/Animation/PixelInstructionProtocol.cs
+++ StellaLib/Network/Protocol/Animation/PixelInstructionProtocol.cs
@@ -3,30 +3,6 @@ using StellaLib.Animation;
 
 namespace StellaLib.Network.Protocol.Animation
 {
-    public class PixelInstructionProtocol
-    {
-        public const int BYTES_NEEDED = sizeof(int) + 1 + 1 + 1; // Index, Red, Green, Blue
-        
-        public static byte[] Serialize(PixelInstruction instruction, byte[] buffer, int startIndex)
-        {
-            BitConverter.GetBytes(instruction.Index).CopyTo(buffer, startIndex);
-            buffer[startIndex + 4] = instruction.R;
-            buffer[startIndex + 5] = instruction.G;
-            buffer[startIndex + 6] = instruction.B;
-            return buffer;
-        }
-
-        public static PixelInstruction Deserialize(byte[] bytes, int startIndex)
-        {
-            PixelInstruction pixelInstruction = new PixelInstruction();
-            pixelInstruction.Index = BitConverter.ToInt32(bytes,startIndex);
-            pixelInstruction.R = bytes[startIndex + 4];
-            pixelInstruction.G = bytes[startIndex + 5];
-            pixelInstruction.B = bytes[startIndex + 6];
-            return pixelInstruction;
-        }
-    }
-
     public class PixelInstructionWithoutDeltaProtocol
     {
         public const int BYTES_NEEDED = 3; // Red, Green, Blue
