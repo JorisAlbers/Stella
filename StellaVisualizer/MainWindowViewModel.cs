9e73082eadf9235111782d4a91b9dac443c016b0
JorisAlbers <joris_albers@hotmail.com>
2019-09-02 21:11:14 +0200

    Added Inits for the cloud and sterk setup

--- StellaVisualizer/MainWindowViewModel.cs
+++ StellaVisualizer/MainWindowViewModel.cs
@@ -19,22 +19,61 @@ namespace StellaVisualizer
 
         public MainWindowViewModel()
         {
+            if (true) // Toggle for the cloud setup
+            {
+                InitAsCloudSetup();
+            }
+
+            if (false) // Toggle for the Sterk setup
+            {
+                InitAsSterkSetup();
+            }
+        }
+           
+
+        private void InitAsCloudSetup()
+        {
             // Start NetworkController which shortcuts the network connection between the server and each client
             _memoryLedStrips = new MemoryLedStrip[3];
             _memoryLedStrips[0] = new MemoryLedStrip(1200);
             _memoryLedStrips[1] = new MemoryLedStrip(1200);
             _memoryLedStrips[2] = new MemoryLedStrip(1200);
             _memoryNetworkController = new MemoryNetworkController(_memoryLedStrips, 1200, 20, 255);
-            
+
+            ServerViewModel = new ServerControlViewModel(_memoryNetworkController);
+
+            Orientation orientation = Orientation.Vertical;
+
+            ClientViewerViewModel[] clientViewModels = new ClientViewerViewModel[3];
+            clientViewModels[0] = new ClientViewerViewModel(1200, orientation);
+            clientViewModels[1] = new ClientViewerViewModel(1200, orientation);
+            clientViewModels[2] = new ClientViewerViewModel(1200, orientation);
+
+            _memoryLedStrips[0].RenderRequested += (sender, colors) => clientViewModels[0].DrawFrame(colors);
+            _memoryLedStrips[1].RenderRequested += (sender, colors) => clientViewModels[1].DrawFrame(colors);
+            _memoryLedStrips[2].RenderRequested += (sender, colors) => clientViewModels[2].DrawFrame(colors);
+
+            ClientsControlViewModel = new ClientsControlViewModel(clientViewModels, orientation);
+        }
+
+        private void InitAsSterkSetup()
+        {
+            // Start NetworkController which shortcuts the network connection between the server and each client
+            _memoryLedStrips = new MemoryLedStrip[3];
+            _memoryLedStrips[0] = new MemoryLedStrip(1200);
+            _memoryLedStrips[1] = new MemoryLedStrip(1200);
+            _memoryLedStrips[2] = new MemoryLedStrip(1200);
+            _memoryNetworkController = new MemoryNetworkController(_memoryLedStrips, 480, 20, 255);
+
             ServerViewModel = new ServerControlViewModel(_memoryNetworkController);
 
             Orientation orientation = Orientation.Horizontal;
 
 
             ClientViewerViewModel[] clientViewModels = new ClientViewerViewModel[3];
-            clientViewModels[0] = new ClientViewerViewModel(1200, orientation);
-            clientViewModels[1] = new ClientViewerViewModel(1200, orientation);
-            clientViewModels[2] = new ClientViewerViewModel(1200, orientation);
+            clientViewModels[0] = new ClientViewerViewModel(480, orientation);
+            clientViewModels[1] = new ClientViewerViewModel(480, orientation);
+            clientViewModels[2] = new ClientViewerViewModel(480, orientation);
 
             _memoryLedStrips[0].RenderRequested += (sender, colors) => clientViewModels[0].DrawFrame(colors);
             _memoryLedStrips[1].RenderRequested += (sender, colors) => clientViewModels[1].DrawFrame(colors);
