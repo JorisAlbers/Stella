f6ac3dc502bb5aca333205ad0338fceca40442bb
JorisAlbers <joris_albers@hotmail.com>
2019-12-01 14:53:57 +0100

    Added ServerConfigurationControl

---
+++ StellaServer/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using StellaServer.ServerConfiguration;
+
+namespace StellaServer
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ServerConfigurationViewModel SeverConfigurationViewModel { get; }
+
+        public MainWindowViewModel()
+        {
+            SeverConfigurationViewModel = new ServerConfigurationViewModel();
+            SeverConfigurationViewModel.SaveRequested += SeverConfigurationViewModel_OnSaveRequested;
+        }
+
+        private void SeverConfigurationViewModel_OnSaveRequested(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StellaServer/ServerConfiguration/ServerConfigurationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StellaServer.ServerConfiguration
+{
+    public class ServerConfigurationViewModel
+    {
+        private ICommand _saveCommand;
+
+        public event EventHandler SaveRequested;
+
+        public string IpAddress { get; set; }
+
+        public int Port { get; set; }
+
+        public string MappingFilePath { get; set; }
+
+        public string StoryboardFolderPath { get; set; }
+
+        public string BitmapFolderPath { get; set; }
+        
+        public ICommand SaveCommand
+        {
+            get { return _saveCommand ??= new RelayCommand((param) => { OnSaveRequested(); }); }
+        }
+
+        public ServerConfigurationViewModel()
+        {
+        }
+
+        private void OnSaveRequested()
+        {
+            SaveRequested?.Invoke(this, new EventArgs());
+        }
+    }
+}
