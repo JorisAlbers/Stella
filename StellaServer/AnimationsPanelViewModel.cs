d05c6bbd8da404fc76fcc814334026a8ff63c414
JorisAlbers <joris_albers@hotmail.com>
2020-07-09 14:28:01 +0200

    Added AnimationsPanel which currently only shows some bitmaps in a folder

--- StellaServer/AnimationsPanelViewModel.cs
+++ StellaServer/AnimationsPanelViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Windows.Media.Imaging;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -10,34 +12,72 @@ namespace StellaServer
 {
     public class AnimationsPanelViewModel : ReactiveObject
     {
+
         [Reactive] public string BitmapFolder { get; set; }
-
-        public extern IEnumerable<string> Bitmaps { [ObservableAsProperty] get; }
-
-
+        
+        public extern IEnumerable<BitmapViewModel> Bitmaps { [ObservableAsProperty] get; }
+        
         public AnimationsPanelViewModel(string bitmapFolder)
         {
             BitmapFolder = bitmapFolder;
 
             this.WhenAnyValue(x => x.BitmapFolder)
-                .Throttle(TimeSpan.FromMilliseconds(100))
+                .Throttle(TimeSpan.FromMilliseconds(1000))
                 .Select(x => x?.Trim())
                 .DistinctUntilChanged()
                 .Select(ScanFolderForBitmaps)
+                .Where(x=> x!=null)
+                .Select(CreateBitmapViewModels)
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .ToPropertyEx(this, x => x.Bitmaps);
         }
 
-        private IEnumerable<string> ScanFolderForBitmaps(string folderPath)
+        private FileInfo[] ScanFolderForBitmaps(string folderPath)
         {
             DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
             if (!directoryInfo.Exists)
             {
+                //throw new ArgumentException($"folder does not exist");
                 Console.Beep();
                 return null;
             }
 
-            return directoryInfo.EnumerateFiles("*.png").Select(x => x.Name).ToArray();
+            return directoryInfo.EnumerateFiles("*.png").ToArray();
+        }
+
+        private IEnumerable<BitmapViewModel> CreateBitmapViewModels(FileInfo[] bitmapFiles)
+        {
+            return bitmapFiles.Select(x => new BitmapViewModel(x.Name, new Bitmap(Bitmap.FromFile(x.FullName))));
+        }
+    }
+
+    public class BitmapViewModel : ReactiveObject
+    {
+        public string Name { get; }
+        public BitmapImage Bitmap { get; }
+
+
+
+        public BitmapViewModel(string name, Bitmap bitmap)
+        {
+            Name = name;
+            Bitmap = BitmapToImageSource(bitmap);
+        }
+
+        private BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
         }
     }
 }
---
+++ StellaServer/BitmapControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace StellaServer
+{
+    /// <summary>
+    /// Interaction logic for BitmapViewModel.xaml
+    /// </summary>
+    public partial class BitmapControl : ReactiveUserControl<BitmapViewModel>
+    {
+        public BitmapControl()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.Bind(ViewModel,
+                        vm => vm.Name,
+                        v => v.Name.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                        vm => vm.Bitmap,
+                        v => v.Image.Source)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
