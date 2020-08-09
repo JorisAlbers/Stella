b4b0fc108858b5c31d5dd55b8edab93513f1e65c
JorisAlbers <joris_albers@hotmail.com>
2020-08-09 14:34:37 +0200

    Skeleton of StatusControl

---
+++ StellaServer/Status/ClientStatusViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace StellaServer.Status
+{
+    public class ClientStatusViewModel : ReactiveObject
+
+    {
+    public string Name { get; }
+
+    [Reactive] public bool IsConnected { get; set; }
+
+    public ClientStatusViewModel(string name)
+    {
+        Name = name;
+    }
+    }
+}
---
+++ StellaServer/Status/StatusViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace StellaServer.Status
+{
+    public class StatusViewModel : ReactiveObject
+    {
+        [Reactive] public ClientStatusViewModel[] Clients { get; set; }
+        [Reactive] public string CurrentlyPlaying { get; set; }
+
+        public StatusViewModel(StellaServerLib.StellaServer stellaServer, int numberOfClients)
+        {
+            Clients = new ClientStatusViewModel[numberOfClients];
+            for (int i = 0; i < numberOfClients; i++)
+            {
+                Clients[i] = new ClientStatusViewModel($"client {i}");
+            }
+        }
+    }
+}
