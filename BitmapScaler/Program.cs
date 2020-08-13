5b5daeb9097e98a06abb1cf38370bcaef3dd32b8
JorisAlbers <joris_albers@hotmail.com>
2020-08-13 20:03:09 +0200

    Recursive search

--- BitmapScaler/Program.cs
+++ BitmapScaler/Program.cs
@@ -7,10 +7,10 @@ namespace BitmapScaler
 {
     class Program
     {
-        private const string BitmapInputDir = @"..\..\..\..\StellaServerConsole\Resources\Bitmaps";
+        private const string BitmapInputDir = @"..\..\..\..\StellaServer\Resources\Bitmaps\Cloud";
         private const string BitmapOutputDir = @"\Bitmaps";
-        private const int _MAX_ROW_SIZE = 360;
-        private const int _MAX_TOTAL_SIZE = 2160;
+        private const int _MAX_ROW_SIZE = 480;
+        private const int _MAX_TOTAL_SIZE = 2880;
 
         static void Main(string[] args)
         {
@@ -19,63 +19,51 @@ namespace BitmapScaler
                 Directory.CreateDirectory(BitmapOutputDir);
             }
 
-            // Iterate main folder
-            foreach (string enumerateFile in Directory.EnumerateFiles(BitmapInputDir))
+            DicSearch(BitmapInputDir);
+        }
+
+        private static void DicSearch(string directory)
+        {
+            foreach (string enumerateFile in Directory.EnumerateFiles(directory))
             {
                 Console.WriteLine($"scaling image {enumerateFile}");
-                Bitmap original = (Bitmap) Image.FromFile(enumerateFile);
+                Bitmap original = (Bitmap)Image.FromFile(enumerateFile);
 
+                Bitmap resized;
                 string fileName = enumerateFile.Split("\\").Last();
                 string nonResizedFileName = fileName.Replace(".png", "_originalSize.png");
-                var resized = new Bitmap(original, new Size(_MAX_ROW_SIZE, original.Height));
-                
 
-                string path = Path.Combine(BitmapOutputDir, fileName);
-                string pathNonResized = Path.Combine(BitmapOutputDir, nonResizedFileName);
+                if (directory.Contains("Full_Setup"))
+                {
+                    resized = new Bitmap(original, new Size(_MAX_TOTAL_SIZE, original.Height));
+                }
+                else
+                {
+                    resized = new Bitmap(original, new Size(_MAX_ROW_SIZE, original.Height));
+                }
+
+                string subFolderPath = directory.Split(BitmapOutputDir).Last();
+                string dirpath = Path.Combine(BitmapOutputDir, subFolderPath);
+
+                if (!Directory.Exists(dirpath))
+                {
+                    Directory.CreateDirectory(dirpath);
+                }
+
+                string path = Path.Combine(dirpath, fileName);
+                string pathNonResized = Path.Combine(dirpath, nonResizedFileName);
+
+
                 Console.Out.WriteLine($"Writing to path {path}");
-
                 resized.Save(path);
                 original.Save(pathNonResized);
             }
 
-            // Iterate subfolders
-            foreach (string enumerateDirectory in Directory.EnumerateDirectories(BitmapInputDir))
+            foreach (string subDir in Directory.EnumerateDirectories(directory))
             {
-                foreach (string enumerateFile in Directory.EnumerateFiles(enumerateDirectory))
-                {
-                    Console.WriteLine($"scaling image {enumerateFile}");
-                    Bitmap original = (Bitmap)Image.FromFile(enumerateFile);
-
-                    Bitmap resized;
-                    string fileName = enumerateFile.Split("\\").Last();
-                    string nonResizedFileName = fileName.Replace(".png", "_originalSize.png");
-
-                    if (enumerateDirectory.Contains("Full_Setup"))
-                    {
-                        resized = new Bitmap(original, new Size(_MAX_TOTAL_SIZE, original.Height));
-                    }
-                    else
-                    {
-                        resized = new Bitmap(original, new Size(_MAX_ROW_SIZE, original.Height));
-                    }
-
-                    string subFolderPath = enumerateDirectory.Split("\\").Last();
-                    string dirpath = Path.Combine(BitmapOutputDir, subFolderPath);
-
-                    if (!Directory.Exists(dirpath))
-                    {
-                        Directory.CreateDirectory(dirpath);
-                    }
-
-                    string path = Path.Combine(dirpath, fileName);
-                    string pathNonResized = Path.Combine(dirpath, nonResizedFileName);
-                    
-
-                    Console.Out.WriteLine($"Writing to path {path}");
-                    resized.Save(path);
-                    original.Save(pathNonResized);
-                }
+                DicSearch(subDir);
             }
+            
         }
     }
 }
