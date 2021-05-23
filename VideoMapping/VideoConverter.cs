37a5c8aeb0d91d43d12041b5d55cb19b9d49941e
JorisAlbers <joris_albers@hotmail.com>
2021-05-23 21:37:19 +0200

    Added skeleton for video converter classes

---
+++ VideoMapping/VideoConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoMapping
+{
+    public class VideosConverter
+    {
+        private readonly DirectoryInfo _inputFolder;
+        private readonly DirectoryInfo _outputFolder;
+
+        public VideosConverter(DirectoryInfo inputFolder, DirectoryInfo outputFolder)
+        {
+            _inputFolder = inputFolder;
+            _outputFolder = outputFolder;
+        }
+    }
+
+    public class VideoConverter
+    {
+        private readonly VideoFile _video;
+        private readonly DirectoryInfo _outputDirectoryInfo;
+
+        public VideoConverter(VideoFile video, DirectoryInfo outputDirectoryInfo)
+        {
+            _video = video;
+            _outputDirectoryInfo = outputDirectoryInfo;
+        }
+    }
+
+    public class VideoFile
+    {
+        public FileInfo FileInfo { get; set; }
+
+        public int ID { get; set; }
+    }
+}
