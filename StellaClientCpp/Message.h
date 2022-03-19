8aa6ce31a6e5500afb1bc08361c7fb470590178f
JorisAlbers <joris_albers@hotmail.com>
2022-03-19 15:24:23 +0100

    Added StellaClientCpp and first implementation of Message.h

---
+++ StellaClientCpp/Message.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <asio.hpp>
+
+namespace stella
+{
+	namespace net
+	{
+		template <typename T>
+		struct message_header
+		{
+			T id{};
+			uint32_t size = 0;
+		};
+
+		template <typename T>
+		struct message
+		{
+			message_header<T> header{ };
+			std::vector<uint8_t> body;
+
+			size_t size() const
+			{
+				return sizeof(message_header<T>) + body.size();
+			}
+
+			friend std::ostream& operator << (std::ostream& os, const message<T>& msg)
+			{
+				os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
+				return os;
+			}
+
+			// TODO we know the length of the message when we create it.
+			// TODO redesign vector to array?
+			// Pushes any POD-like data into the message buffer
+			template<typename DataType>
+			friend message<T>& operator << (message<t>& msg, const DataType& data)
+			{
+				// Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
+
+				// Cache current size of vector, as this will be the point we insert the data
+				size_t i = msg.body.size();
+
+				// Resize the vector by the size of the data being pushed
+				msg.body.resize(msg.body.size() + sizeof(DataType));
+
+				// Physically copy the data into the newly allocated vector space
+				std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+
+				// Recalculate the message size
+				msg.header.size = msg.size();
+
+				// Return the target message so it can be "chained"
+				return msg;
+			}
+
+			// Pulls any POD-like data form the message buffer
+			template<typename DataType>
+			friend message<T>& operator >> (message<T>& msg, DataType& data)
+			{
+				// Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pulled from vector");
+
+				// Cache the location towards the end of the vector where the pulled data starts
+				size_t i = msg.body.size() - sizeof(DataType);
+
+				// Physically copy the data from the vector into the user variable
+				std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+
+				// Shrink the vector to remove read bytes, and reset end position
+				msg.body.resize(i);
+
+				// Recalculate the message size
+				msg.header.size = msg.size();
+
+				// Return the target message so it can be "chained"
+				return msg;
+			}
+
+		};
+	}
+}
+
