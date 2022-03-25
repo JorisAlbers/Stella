3ac1c316b72811f92f953fef4848e141c05d5f9d
JorisAlbers <joris_albers@hotmail.com>
2022-03-25 16:12:18 +0100

    Added message out, which has a small buffer

--- StellaClientCpp/Message.h
+++ StellaClientCpp/Message.h
@@ -6,7 +6,7 @@ namespace stella
 	namespace net
 	{
 		const int UDP_HEADER_SIZE = 60000;
-
+		const int MAX_MESSAGE_OUT_SIZE = sizeof(int) + sizeof(int);
 
 		enum class StellaMessageTypes : uint32_t
 		{
@@ -59,6 +59,49 @@ namespace stella
 
 				msg.header.size = sizeof(int) + msg.currentIndex; // int = message type 
 				
+				// Return the target message so it can be "chained"
+				return msg;
+			}
+		};
+
+		/// <summary>
+		/// An out message is small. Only a small buffer needed.
+		/// </summary>
+		struct message_out 
+		{
+			message_header header{ };
+			char body[MAX_MESSAGE_OUT_SIZE];
+			int currentIndex = 0;
+
+
+			size_t size() const
+			{
+				return sizeof(header.size) + header.size; // size excludes itself.
+			}
+
+			friend std::ostream& operator << (std::ostream& os, const message_out& msg)
+			{
+				os << "ID:" << int(msg.header.type) << " Size:" << msg.header.size;
+				return os;
+			}
+
+			// TODO we know the length of the message when we create it.
+			// TODO redesign vector to array?
+			// Pushes any POD-like data into the message buffer
+			template<typename DataType>
+			friend message_out& operator << (message_out& msg, const DataType& data)
+			{
+				// Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
+
+				// copy data
+				std::memcpy(&msg.body[msg.currentIndex], &data, sizeof(DataType));
+
+				// increment index
+				msg.currentIndex += sizeof(DataType);
+
+				msg.header.size = sizeof(int) + msg.currentIndex; // int = message type 
+
 				// Return the target message so it can be "chained"
 				return msg;
 			}
