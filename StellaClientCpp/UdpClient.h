bd3a970cbcbc6f4a0b77fc12cc535c3df694d5e4
JorisAlbers <joris_albers@hotmail.com>
2022-03-27 22:08:06 +0200

    Implemented reading of udp data

--- StellaClientCpp/UdpClient.h
+++ StellaClientCpp/UdpClient.h
@@ -24,14 +24,13 @@ namespace stella
 			// store the part assembled message here, until it is ready
 			message_in_udp m_msgTemporaryIn;
 
-			asio::ip::udp::resolver::results_type m_endpoints;
+			asio::ip::udp::endpoint m_local_endpoint;
 
 
 		public:
 			explicit UdpClient(const std::string& host, const uint16_t port)
 			{
-				asio::ip::udp::resolver resolver(m_context);
-				m_endpoints = resolver.resolve(host, std::to_string(port));
+				m_local_endpoint = asio::ip::udp::endpoint(asio::ip::address::from_string(host), port);				
 			}
 
 			virtual ~UdpClient()
@@ -46,20 +45,12 @@ namespace stella
 				try
 				{
 					m_socket = new asio::ip::udp::socket(m_context);
+					m_socket->open(asio::ip::udp::v4());
+					m_socket->bind(m_local_endpoint);
+							
+					ReadHeader();
 
-					asio::async_connect(*m_socket,m_endpoints,
-						[this](std::error_code ec, asio::ip::udp::endpoint endpoint)
-						{
-							if (ec)
-							{
-								std::cerr << "Failed to connect to server at " << " \n";
-								std::cerr << ec.message() << "\n";
-								return;
-							}
-						});
-
-					threadContext = std::thread([this]() {m_context.run();});
-
+					threadContext = std::thread([this]() {m_context.run();});				
 				}
 				catch (std::exception& e)
 				{
@@ -84,82 +75,83 @@ namespace stella
 				return m_socket->is_open();
 			};
 
-		//private:
-		//	void ReadHeader()
-		//	{
-		//		asio::async_read(*m_socket, asio::buffer(&m_msgTemporaryIn.header, sizeof(message_header)),
-		//			[this](std::error_code ec, std::size_t length)
-		//			{
-		//				if (!ec)
-		//				{
-		//					// A complete message header has been read, check if this message
-		//					// has a body to follow...
-		//					if (m_msgTemporaryIn.header.size > 0)
-		//					{
-		//						ReadBody();
-		//					}
-		//					else
-		//					{
-		//						if (m_msgTemporaryIn.header.type == StellaMessageTypes::Unknown)
-		//						{
-		//							// Keep alive message received.
-		//							std::cout << "Keep alive message received." << std::endl;
-		//							ReadHeader();
-		//							return;
-		//						}
+		private:
+			void ReadHeader()
+			{
+				m_socket->async_receive(asio::buffer(&m_msgTemporaryIn.header, sizeof(message_header)),
+					[this](std::error_code ec, std::size_t length)
+					{
+						if (!ec)
+						{
+							// A complete message header has been read, check if this message
+							// has a body to follow...
+							if (m_msgTemporaryIn.header.size > 0)
+							{
+								ReadBody();
+							}
+							else
+							{
+								if (m_msgTemporaryIn.header.type == StellaMessageTypes::Unknown)
+								{
+									// Keep alive message received.
+									std::cout << "Keep alive message received." << std::endl;
+									ReadHeader();
+									return;
+								}
 
-		//						// it doesn't, so add this bodyless message to the connections
-		//						// incoming message queue
-		//						AddToIncomingMessageQueue();
-		//					}
-		//				}
-		//				else
-		//				{
-		//					int id = 10;
-		//					// Reading form the client went wrong, most likely a disconnect
-		//					// has occurred. Close the socket and let the system tidy it up later.
-		//					std::cout << "[" << id << "] Read Header Fail.\n";
-		//					std::cout << ec.message() << "\n";
-		//					m_socket->close();
-		//				}
-		//			});
-		//	}
+								// it doesn't, so add this bodyless message to the connections
+								// incoming message queue
+								AddToIncomingMessageQueue();
+							}
+						}
+						else
+						{
+							int id = 10;
+							// Reading form the client went wrong, most likely a disconnect
+							// has occurred. Close the socket and let the system tidy it up later.
+							std::cout << "[" << id << "] Read Header Fail.\n";
+							std::cout << ec.message() << "\n";
+							m_socket->close();
+						}
 
-		//	// ASYNC - Prime context ready to read a message body
-		//	void ReadBody()
-		//	{
-		//		// If this function is called, a header has already been read, and that header
-		//		// request we read a body, The space for that body has already been allocated
-		//		// in the temporary message object, so just wait for the bytes to arrive...
-		//		asio::async_read(*m_socket, asio::buffer(m_msgTemporaryIn.body, UDP_BUFFER_SIZE),
-		//			[this](std::error_code ec, std::size_t length)
-		//			{
-		//				if (!ec)
-		//				{
-		//					// ...and they have! The message is now complete, so add
-		//					// the whole message to incoming queue
-		//					AddToIncomingMessageQueue();
-		//				}
-		//				else
-		//				{
-		//					// As above!
-		//					int id = 10;
-		//					std::cout << "[" << id << "] Read Body Fail.\n";
-		//					std::cout << "Reason: " << ec.message() << std::endl;
-		//					m_socket->close();
-		//				}
-		//			});
-		//	}
+					});
+			}
 
-		//	// Once a full message is received, add it to the incoming queue
-		//	void AddToIncomingMessageQueue()
-		//	{
-		//		// Shove it in queue, converting it to an "owned message", by initialising
-		//		// with the a shared pointer from this connection object
-		//		queueIn.push_back(m_msgTemporaryIn);
+			// ASYNC - Prime context ready to read a message body
+			void ReadBody()
+			{
+				// If this function is called, a header has already been read, and that header
+				// request we read a body, The space for that body has already been allocated
+				// in the temporary message object, so just wait for the bytes to arrive...
+				m_socket-> async_receive(asio::buffer(m_msgTemporaryIn.body, UDP_BUFFER_SIZE),
+					[this](std::error_code ec, std::size_t length)
+					{
+						if (!ec)
+						{
+							// ...and they have! The message is now complete, so add
+							// the whole message to incoming queue
+							AddToIncomingMessageQueue();
+						}
+						else
+						{
+							// As above!
+							int id = 10;
+							std::cout << "[" << id << "] Read Body Fail.\n";
+							std::cout << "Reason: " << ec.message() << std::endl;
+							m_socket->close();
+						}
+					});
+			}
 
-		//		ReadHeader();
-		//	}
+			// Once a full message is received, add it to the incoming queue
+			void AddToIncomingMessageQueue()
+			{
+				// Shove it in queue, converting it to an "owned message", by initialising
+				// with the a shared pointer from this connection object
+				queueIn.push_back(m_msgTemporaryIn);
+
+				ReadHeader();
+			}
 
 
 		};
