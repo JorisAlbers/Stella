184f78a39fc896c0a37ad3ef4a04d797dc94291c
JorisAlbers <joris_albers@hotmail.com>
2022-04-10 11:11:20 +0200

    Get midi devices

--- StellaServer/Midi/MidiInputManager.cs
+++ StellaServer/Midi/MidiInputManager.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using ReactiveUI;
+using NAudio.Midi;
 
 namespace StellaServer.Midi
 {
     public class MidiInputManager : ReactiveObject
     {
+        private static List<MidiWithIndex> GetDevices()
+        {
+            List<MidiWithIndex> devices = new List<MidiWithIndex>();
+            
+            for (int index = 0; index < MidiIn.NumberOfDevices; index++)
+            {
+                MidiInCapabilities x = MidiIn.DeviceInfo(index);
+                devices.Add(new MidiWithIndex(x, index));
+            }
 
+            return devices;
+        }
+    }
+
+    public class MidiWithIndex
+    {
+        public MidiInCapabilities Device { get; }
+        public int Index { get; }
+
+        public MidiWithIndex(MidiInCapabilities device, int index)
+        {
+            Device = device;
+            Index = index;
+        }
     }
 }
