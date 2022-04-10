fb4a6af426c16839069af59c1f024d00f70ef211
JorisAlbers <joris_albers@hotmail.com>
2022-04-10 11:07:24 +0200

    Added skeleton for midi input

---
+++ StellaServer/Midi/MidiInputManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace StellaServer.Midi
+{
+    public class MidiInputManager : ReactiveObject
+    {
+
+    }
+}
