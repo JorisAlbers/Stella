e409884393bb4831d825901fb34767edc7a4f333
JorisAlbers <joris_albers@hotmail.com>
2022-04-22 10:40:53 +0200

    Revert "Removed todos"
    This reverts commit ca237a027c34079c0d803cdf629d9f028d4740a1.

--- StellaClientCpp/Message.h
+++ StellaClientCpp/Message.h
@@ -43,6 +43,8 @@ namespace stella
 				return os;
 			}
 
+			// TODO we know the length of the message when we create it.
+			// TODO redesign vector to array?
 			// Pushes any POD-like data into the message buffer
 			template<typename DataType>
 			friend message_in_tcp& operator << (message_in_tcp& msg, const DataType& data)
@@ -81,6 +83,8 @@ namespace stella
 				return os;
 			}
 
+			// TODO we know the length of the message when we create it.
+			// TODO redesign vector to array?
 			// Pushes any POD-like data into the message buffer
 			template<typename DataType>
 			friend message_in_udp& operator << (message_in_udp& msg, const DataType& data)
@@ -122,6 +126,8 @@ namespace stella
 				return os;
 			}
 
+			// TODO we know the length of the message when we create it.
+			// TODO redesign vector to array?
 			// Pushes any POD-like data into the message buffer
 			template<typename DataType>
 			friend message_out& operator << (message_out& msg, const DataType& data)
