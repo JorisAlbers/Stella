b693f0f6d0e99638cbc1d5802c61debb1aa12076
JorisAlbers <joris_albers@hotmail.com>
2022-05-14 18:51:31 +0200

    Added ConnectionRequestProtocol and ConnectionRequestMessage

---
+++ StellaLib/Network/Protocol/ConnectionRequestProtocol.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StellaLib.Animation;
+
+namespace StellaLib.Network.Protocol
+{
+    class ConnectionRequestProtocol
+    {
+        public const int BYTES_NEEDED = sizeof(byte) *2; // key, version
+
+        public static ConnectionRequestMessage Deserialize(byte[] bytes, int startIndex)
+        {
+            return new ConnectionRequestMessage(bytes[startIndex], bytes[startIndex+1]);
+        }
+    }
+
+    public class ConnectionRequestMessage
+    {
+        public byte Key { get; }
+        public byte Version { get; }
+
+        public ConnectionRequestMessage(byte key, byte version)
+        {
+            Key = key;
+            Version = version;
+        }
+    }
+}
