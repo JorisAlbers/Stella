291588ee8192cc6d0d9c4c0110ae94314d345543
Joris Albers <joris_albers@hotmail.com>
2024-06-27 12:36:12 +0200

    Added a textbox for the bpm and the interval

--- StellaServer/BpmControl.xaml.cs
+++ StellaServer/BpmControl.xaml.cs
@@ -1,3 +1,6 @@
+using System.Reactive.Disposables;
+using ReactiveUI;
+
 namespace StellaServer
 {
     public partial class BpmControl
@@ -5,6 +8,19 @@ namespace StellaServer
         public BpmControl()
         {
             InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(ViewModel,
+                        vm => vm.Bpm,
+                        v => v.BpmTextBlock.Text)
+                    .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                        vm => vm.Interval,
+                        v => v.IntervalTextBlock.Text)
+                    .DisposeWith(d);
+            });
         }
     }
 }
